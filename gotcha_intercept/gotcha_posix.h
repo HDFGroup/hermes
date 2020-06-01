49516a7496edeaed8fc519faac8fe1187ae511e4
Kimmy Mu <kmu@hdfgroup.org>
2020-06-01 16:16:05 -0400

    reorganize to avoid template with C linkage error

--- gotcha_intercept/gotcha_posix.h
+++ gotcha_intercept/gotcha_posix.h
@@ -1,41 +1,6 @@
 #ifndef GOTCHA_POSIX_H_
 #define GOTCHA_POSIX_H_
 
-#include <unistd.h>
-
-#include "gotcha/gotcha.h"
-#include "gotcha/gotcha_types.h"
-
-#define FUNC_COUNT(wraps) ((sizeof(wraps) / sizeof(wraps[0])))
-
-gotcha_wrappee_handle_t orig_pwrite_handle;
-typedef ssize_t (hermes_pwrite_fp)(int fd, const void *buf, size_t count, off_t offset);
-ssize_t hermes_pwrite(int fd, const void *buf, size_t count, off_t offset);
-
-gotcha_wrappee_handle_t orig_pread_handle;
-typedef ssize_t (hermes_pread_fp)(int fd, void *buf, size_t count, off_t offset);
-ssize_t hermes_pread(int fd, void *buf, size_t count, off_t offset);
-
-gotcha_wrappee_handle_t orig_lseek_handle;
-typedef off_t (hermes_lseek_fp)(int fd, off_t offset, int whence);
-off_t hermes_lseek(int fd, off_t offset, int whence);
-
-gotcha_wrappee_handle_t orig_write_handle;
-typedef ssize_t (hermes_write_fp)(int fd, const void *buf, size_t count);
-ssize_t hermes_write(int fd, const void *buf, size_t count);
-
-gotcha_wrappee_handle_t orig_read_handle;
-typedef ssize_t (hermes_read_fp)(int fd, void *buf, size_t count);
-ssize_t hermes_read(int fd, void *buf, size_t count);
-
-struct gotcha_binding_t wrap_posix [] = {
-        { "pwrite", (void *)hermes_pwrite, &orig_pwrite_handle },
-        { "pread", (void *)hermes_pread, &orig_pread_handle },
-        { "lseek", (void *)hermes_lseek, &orig_lseek_handle },
-        { "write", (void *)hermes_write, &orig_write_handle },
-        { "read", (void *)hermes_read, &orig_read_handle },
-};
-
 void init_gotcha_posix ();
 
 #endif  // GOTCHA_POSIX_H_
--- gotcha_intercept/gotcha_stdio.cc
+++ gotcha_intercept/gotcha_stdio.cc
@@ -2,6 +2,41 @@ extern "C" {
 #include "gotcha_stdio.h"
 }
 
+#include <stdio.h>
+
+#include "gotcha/gotcha.h"
+#include "gotcha/gotcha_types.h"
+
+#define FUNC_COUNT(wraps) ((sizeof(wraps) / sizeof(wraps[0])))
+
+gotcha_wrappee_handle_t orig_fopen_handle;
+typedef FILE * (hermes_fopen_fp)(const char *filename, const char * mode);
+FILE * hermes_fopen(const char *filename, const char * mode);
+
+gotcha_wrappee_handle_t orig_fclose_handle;
+typedef int (hermes_fclose_fp)(FILE *stream);
+int hermes_fclose(FILE *stream);
+
+gotcha_wrappee_handle_t orig_fwrite_handle;
+typedef size_t (hermes_fwrite_fp)(const void *ptr, size_t size, size_t count, FILE *stream);
+size_t hermes_fwrite( const void *ptr, size_t size, size_t count, FILE *stream);
+
+gotcha_wrappee_handle_t orig_fread_handle;
+typedef size_t (hermes_fread_fp)(void * ptr, size_t size, size_t count, FILE *stream);
+size_t hermes_fread(void * ptr, size_t size, size_t count, FILE *stream);
+
+gotcha_wrappee_handle_t orig_fseek_handle;
+typedef size_t (hermes_fseek_fp)(FILE * stream, long int offset, int origin);
+size_t hermes_fseek(FILE * stream, long int offset, int origin);
+
+struct gotcha_binding_t wrap_stdio [] = {
+        { "fopen", (void *)hermes_fopen, &orig_fopen_handle },
+        { "fclose", (void *)hermes_fclose, &orig_fclose_handle },
+        { "fwrite", (void *)hermes_fwrite, &orig_fwrite_handle },
+        { "fread", (void *)hermes_fread, &orig_fread_handle },
+        { "fseek", (void *)hermes_fseek, &orig_fseek_handle },
+};
+
 void init_gotcha_stdio() {
   gotcha_wrap(wrap_stdio, FUNC_COUNT(wrap_stdio), "hermes");
 }
