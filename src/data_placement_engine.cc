2c511fdb2d14e0ab91699e716b9b91e6624fdaad
Kimmy Mu <kmu@hdfgroup.org>
2020-08-24 23:27:18 -0400

    modify error message

--- src/data_placement_engine.cc
+++ src/data_placement_engine.cc
@@ -137,7 +137,7 @@ Status RandomPlacement(SharedMemoryContext *context, RpcContext *rpc,
         auto itlow = ordered_cap.lower_bound (new_blob_size[k]);
         if (itlow == ordered_cap.end()) {
           HERMES_NOT_IMPLEMENTED_YET;
-          assert(!"No target has enough capacity\n");
+          assert(!"No buffer device has enough capacity! Go to PFS.\n");
         }
 
         std::uniform_int_distribution<std::mt19937::result_type>
