317817c47ab5f7499d963d67cb2542bba7420a2c
Chris Hogan <c096h800@gmail.com>
2020-10-09 09:51:41 -0500

    Timing get and delete

--- benchmarks/mdm_bench.cc
+++ benchmarks/mdm_bench.cc
@@ -6,6 +6,18 @@
 #include "utils.h"
 
 namespace hapi = hermes::api;
+using std::chrono::time_point;
+
+double GetMaxSeconds(time_point<std::chrono::high_resolution_clock> start,
+                     time_point<std::chrono::high_resolution_clock> end,
+                     hapi::Hermes *hermes) {
+  double seconds = std::chrono::duration<double>(end - start).count();
+  double max_seconds = 0;
+  MPI_Comm *app_comm = (MPI_Comm *)hermes->GetAppCommunicator();
+  MPI_Reduce(&seconds, &max_seconds, 1, MPI_DOUBLE, MPI_MAX, 0, *app_comm);
+
+  return max_seconds;
+}
 
 int main(int argc, char **argv) {
   int mpi_threads_provided;
@@ -43,24 +55,38 @@ int main(int argc, char **argv) {
         buffer_ids[j] = id;
       }
 
-      auto start = now();
+      time_point start_put = now();
       hermes::u32 location = LocalAllocateBufferIdList(mdm, buffer_ids);
-      auto end = now();
+      time_point end_put = now();
 
       hermes::BlobID blob_id = {};
       blob_id.bits.node_id = app_rank;
       blob_id.bits.buffer_ids_offset = location;
+
+      hermes::ScopedTemporaryMemory scratch(&hermes->trans_arena_);
+      hermes::BufferIdArray buffer_id_arr = {};
+
+      time_point start_get = now();
+      hermes::LocalGetBufferIdList(scratch, mdm, blob_id, &buffer_id_arr);
+      time_point end_get = now();
+
+      time_point start_del = now();
       LocalFreeBufferIdList(&hermes->context_, blob_id);
+      time_point end_del = now();
 
       hermes->AppBarrier();
 
-      double seconds = std::chrono::duration<double>(end - start).count();
-      double max_seconds = 0;
-      MPI_Comm *app_comm = (MPI_Comm *)hermes->GetAppCommunicator();
-      MPI_Reduce(&seconds, &max_seconds, 1, MPI_DOUBLE, MPI_MAX, 0, *app_comm);
+      double max_put_seconds = GetMaxSeconds(start_put, end_put, hermes.get());
+      double max_get_seconds = GetMaxSeconds(start_get, end_get, hermes.get());
+      double max_del_seconds = GetMaxSeconds(start_del, end_del, hermes.get());
 
       if (hermes->comm_.first_on_node) {
-        printf("put,local,1,%d,%d,%f\n", app_size, payload_bytes, max_seconds);
+        printf("put,local,1,%d,%d,%.8f\n", app_size, payload_bytes,
+               max_put_seconds);
+        printf("get,local,1,%d,%d,%.8f\n", app_size, payload_bytes,
+               max_get_seconds);
+        printf("del,local,1,%d,%d,%.8f\n", app_size, payload_bytes,
+               max_del_seconds);
       }
     }
 
