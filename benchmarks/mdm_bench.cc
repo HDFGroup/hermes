69bd8a0a867effd9cb789a19691cf7248815e599
Chris Hogan <c096h800@gmail.com>
2020-10-16 15:53:08 -0500

    No throughput for timing delete

--- benchmarks/mdm_bench.cc
+++ benchmarks/mdm_bench.cc
@@ -145,8 +145,7 @@ void BenchRemote(const char *config_file) {
         double payload_megabytes = (payload_bytes * kRepeats) / 1024.0 / 1024.0;
         printf("put,%d,%d,%f,%f\n", app_size, payload_bytes, kRepeats / put_seconds,
                payload_megabytes / put_seconds);
-        printf("del,%d,%d,%f,%f\n", app_size, payload_bytes, kRepeats / del_seconds,
-               payload_megabytes / del_seconds);
+        printf("del,%d,%d,%f\n", app_size, payload_bytes, kRepeats / del_seconds);
         // printf("put,remote,1,1,%d,%.8f\n", payload_bytes, put_seconds);
         // printf("get,local,1,%d,%d,%.8f\n", app_size, payload_bytes,
         //        max_get_seconds);
