a2dc0ca94c8a387987457e39fd11d9b02ea6ac3a
hariharan devarajan <mani.hariharan@gmail.com>
2020-12-22 15:18:54 -0600

    added test

--- adapter/test/posix/posix_adapter_shared_test.cpp
+++ adapter/test/posix/posix_adapter_shared_test.cpp
@@ -44,9 +44,11 @@ TEST_CASE("SharedFile",
                 long cur_offset = lseek(fd, bytes_read, SEEK_SET);
                 REQUIRE(cur_offset == bytes_read);
                 if (file_size > bytes_read) {
+                    long read_size = args.request_size < file_size - bytes_read ?
+                                     args.request_size : file_size - bytes_read;
                     auto read_bytes = read(fd, info.read_data.data(),
-                                           args.request_size);
-                    REQUIRE(read_bytes == args.request_size);
+                                           read_size);
+                    REQUIRE(read_bytes == read_size);
                     bytes_read += read_bytes;
                 }
                 lock.l_type = F_UNLCK;
