52548e276d2a89214420746128a9074a87a9cd25
Kimmy Mu <kmu@hdfgroup.org>
2021-01-07 23:16:56 -0500

    move error handling from bucket.cc to dpe

--- src/data_placement_engine.cc
+++ src/data_placement_engine.cc
@@ -381,6 +381,7 @@ Status CalculatePlacement(SharedMemoryContext *context, RpcContext *rpc,
   // Aggregate placement schemas from the same target
   for (auto it = output_tmp.begin(); it != output_tmp.end(); ++it) {
     PlacementSchema schema = AggregateBlobSchema((*it));
+    CHECK(schema.size() > 0) << "PlacementSchema is empty";
     output.push_back(schema);
   }
 
