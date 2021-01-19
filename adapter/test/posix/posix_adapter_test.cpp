c45fe5b62b33e913e167d03f961d2940232b9444
Hariharan Devarajan <mani.hariharan@gmail.com>
2021-01-19 03:07:35 -0600

    fixed test cases.

--- adapter/test/posix/posix_adapter_test.cpp
+++ adapter/test/posix/posix_adapter_test.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <experimental/filesystem>
-#include <unistd.h>
-#include <fcntl.h>
 #include <catch_config.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <experimental/filesystem>
+#include <iostream>
 #ifndef O_TMPFILE
 #define __O_TMPFILE 020000000
 #define O_TMPFILE (__O_TMPFILE | O_DIRECTORY)
@@ -12,78 +13,73 @@ namespace fs = std::experimental::filesystem;
 
 namespace hermes::adapter::posix::test {
 struct Arguments {
-    std::string filename = "test.dat";
-    std::string directory = "/tmp";
-    size_t request_size = 65536;
+  std::string filename = "test.dat";
+  std::string directory = "/tmp";
+  size_t request_size = 65536;
 };
 struct Info {
-    int rank = 0;
-    int comm_size = 1;
-    std::string write_data;
-    std::string read_data;
-    std::string new_file;
-    std::string existing_file;
-    size_t num_iterations = 1024;
-    unsigned int offset_seed = 1;
-    unsigned int rs_seed = 1;
-    unsigned int temporal_interval_seed = 5;
-    size_t total_size;
-    size_t stride_size = 1024;
-    unsigned int temporal_interval_ms = 1;
-    size_t small_min = 1, small_max = 4 * 1024;
-    size_t medium_min = 4 * 1024 + 1,
-            medium_max = 256 * 1024;
-    size_t large_min = 256 * 1024 + 1,
-            large_max = 4 * 1024 * 1024;
+  int rank = 0;
+  int comm_size = 1;
+  std::string write_data;
+  std::string read_data;
+  std::string new_file;
+  std::string existing_file;
+  size_t num_iterations = 64;
+  unsigned int offset_seed = 1;
+  unsigned int rs_seed = 1;
+  unsigned int temporal_interval_seed = 5;
+  size_t total_size;
+  size_t stride_size = 1024;
+  unsigned int temporal_interval_ms = 1;
+  size_t small_min = 1, small_max = 4 * 1024;
+  size_t medium_min = 4 * 1024 + 1, medium_max = 256 * 1024;
+  size_t large_min = 256 * 1024 + 1, large_max = 3 * 1024 * 1024;
 };
 }  // namespace hermes::adapter::posix::test
 hermes::adapter::posix::test::Arguments args;
 hermes::adapter::posix::test::Info info;
 
 int init() {
-    info.write_data = std::string(args.request_size, 'w');
-    info.read_data = std::string(args.request_size, 'r');
-    return 0;
+  info.write_data = std::string(args.request_size, 'w');
+  info.read_data = std::string(args.request_size, 'r');
+  return 0;
 }
-int finalize() {
-    return 0;
-}
+int finalize() { return 0; }
 
 int pretest() {
-    fs::path fullpath = args.directory;
-    fullpath /= args.filename;
-    info.new_file = fullpath.string() + "_new";
-    info.existing_file = fullpath.string() + "_ext";
-    if (fs::exists(info.new_file)) fs::remove(info.new_file);
-    if (fs::exists(info.existing_file)) fs::remove(info.existing_file);
-    if (!fs::exists(info.existing_file)) {
-        std::string cmd = "dd if=/dev/zero of="+info.existing_file+
-                          " bs=1 count=0 seek="+
-                          std::to_string(args.request_size
-                                         * info.num_iterations)
-                          + " > /dev/null 2>&1";
-        system(cmd.c_str());
-        REQUIRE(fs::file_size(info.existing_file)
-                == args.request_size * info.num_iterations);
-        info.total_size = fs::file_size(info.existing_file);
-    }
-    REQUIRE(info.total_size > 0);
-    return 0;
+  fs::path fullpath = args.directory;
+  fullpath /= args.filename;
+  info.new_file = fullpath.string() + "_new";
+  info.existing_file = fullpath.string() + "_ext";
+  if (fs::exists(info.new_file)) fs::remove(info.new_file);
+  if (fs::exists(info.existing_file)) fs::remove(info.existing_file);
+  if (!fs::exists(info.existing_file)) {
+    std::string cmd = "dd if=/dev/zero of=" + info.existing_file +
+                      " bs=1 count=0 seek=" +
+                      std::to_string(args.request_size * info.num_iterations) +
+                      " > /dev/null 2>&1";
+    system(cmd.c_str());
+    REQUIRE(fs::file_size(info.existing_file) ==
+            args.request_size * info.num_iterations);
+    info.total_size = fs::file_size(info.existing_file);
+  }
+  REQUIRE(info.total_size > 0);
+  return 0;
 }
 
 int posttest() {
-    if (fs::exists(info.new_file)) fs::remove(info.new_file);
-    if (fs::exists(info.existing_file)) fs::remove(info.existing_file);
-    return 0;
+  if (fs::exists(info.new_file)) fs::remove(info.new_file);
+  if (fs::exists(info.existing_file)) fs::remove(info.existing_file);
+  return 0;
 }
 
 cl::Parser define_options() {
-    return cl::Opt(args.filename, "filename")["-f"]["--filename"]
-                   ("Filename used for performing I/O")
-           | cl::Opt(args.directory, "dir")["-d"]["--directory"]
-                   ("Directory used for performing I/O")
-           | cl::Opt(args.request_size, "request_size")["-s"]["--request_size"]
-                   ("Request size used for performing I/O");
+  return cl::Opt(args.filename, "filename")["-f"]["--filename"](
+             "Filename used for performing I/O") |
+         cl::Opt(args.directory, "dir")["-d"]["--directory"](
+             "Directory used for performing I/O") |
+         cl::Opt(args.request_size, "request_size")["-s"]["--request_size"](
+             "Request size used for performing I/O");
 }
 
 #include "posix_adapter_basic_test.cpp"
