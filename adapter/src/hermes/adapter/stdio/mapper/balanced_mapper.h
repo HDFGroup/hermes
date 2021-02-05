37e7f8a15a13e8d3c1943300f9c19d9843411bce
Kimmy Mu <kmu@hdfgroup.org>
2021-02-05 10:50:45 -0500

    fix copyright for more files

--- adapter/src/hermes/adapter/stdio/mapper/balanced_mapper.h
+++ adapter/src/hermes/adapter/stdio/mapper/balanced_mapper.h
@@ -6,8 +6,8 @@
  *                                                                           *
  * This file is part of Hermes. The full Hermes copyright notice, including  *
  * terms governing use, modification, and redistribution, is contained in    *
- * the COPYFILE, which can be found at the top directory. If you do not have *
- * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef HERMES_BALANCED_MAPPER_H
