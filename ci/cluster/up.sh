4224bac27df96479d2324e634b4b46dbf1be2bea
Chris Hogan <c096h800@gmail.com>
2021-02-17 15:41:17 -0600

    Simplify docker-compose.yml

--- ci/cluster/up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for n in node1 node2; do
-    docker-compose build --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) ${n}
-done
-
-docker-compose up -d --scale node1=1 --scale node2=1 --no-recreate
