ecef3be0b07efbe3c8ca417c9fbbfafb93f3cb22
Kimmy Mu <kmu@hdfgroup.org>
2021-02-27 19:16:55 -0500

    git test back

--- test/mdm_test.cc
+++ test/mdm_test.cc
@@ -233,6 +233,7 @@ int main(int argc, char **argv) {
   TestRenameBucket(hermes);
   TestBucketRefCounting(hermes);
   TestMaxNameLength(hermes);
+  TestGetRelativeNodeId();
 
   hermes->Finalize(true);
 
