f3c6b8f181717d8caa901cf62a98512d805c265b
Chris Hogan <c096h800@gmail.com>
2021-04-23 16:20:27 -0500

    VPIC bench arguments

--- benchmarks/vpic_bench.cc
+++ benchmarks/vpic_bench.cc
@@ -1,6 +1,7 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <unistd.h>
 
 #include <algorithm>
 #include <chrono>
@@ -13,31 +14,136 @@
 namespace hapi = hermes::api;
 using u8 = hermes::u8;
 
+struct Options {
+  int num_buckets;
+  int num_iterations;
+  size_t num_particles;
+  bool do_posix_io;
+  char *output_file;
+};
+
+void PrintUsage(char *program) {
+  fprintf(stderr, "Usage: %s Doc string goes here\n", program);
+  fprintf(stderr, "  -b\n");
+  fprintf(stderr, "    doc\n");
+  fprintf(stderr, "  -i\n");
+  fprintf(stderr, "    doc\n");
+  fprintf(stderr, "  -o\n");
+  fprintf(stderr, "    doc\n");
+  fprintf(stderr, "  -p\n");
+  fprintf(stderr, "    doc\n");
+  fprintf(stderr, "  -x\n");
+  fprintf(stderr, "    doc\n");
+}
+
+Options HandleArgs(int argc, char **argv) {
+  Options result = {};
+  result.num_buckets = 1;
+  result.num_iterations = 16;
+  result.num_particles = (8 * 1024 * 1024) / sizeof(float);
+  result.output_path = "./";
+
+  int option = -1;
+  while ((option = getopt(argc, argv, "b:i:o:p:h")) != -1) {
+    switch (option) {
+      case 'h': {
+        PrintUsage(argv[0]);
+        exit(0);
+      }
+      case 'b': {
+        result.num_buckets = atoi(optarg);
+        break;
+      }
+      case 'i': {
+        result.num_iterations = atoi(optarg);
+        break;
+      }
+      case 'o': {
+        result.output_file = optarg;
+        break;
+      }
+      case 'p': {
+        result.num_particles = (size_t)std::stoull(optarg);
+        break;
+      }
+      case 'x': {
+        result.do_posix_io = true;
+        break;
+      }
+      default: {
+        PrintUsage(argv[0]);
+        exit(1);
+      }
+    }
+  }
+  if (optind < argc) {
+    fprintf(stderr, "non-option ARGV-elements: ");
+    while (optind < argc) {
+      fprintf(stderr, "%s ", argv[optind++]);
+    }
+    fprintf(stderr, "\n");
+  }
+
+  return result;
+}
+
 static inline double uniform_random_number() {
   return (((double)rand())/((double)(RAND_MAX)));
 }
 
-static void MasterPrintf(int rank, const char *msg) {
-  if (rank == 0) {
-    printf("%s", msg);
-  }
-}
-
 template<typename T>
-void DoFwrite(const std::vector<T> &vec, FILE *f) {
+static void DoFwrite(const std::vector<T> &vec, FILE *f) {
   size_t total_bytes = vec.size() * sizeof(T);
   size_t bytes_written = fwrite(vec.data(), 1, total_bytes, f);
   CHECK_EQ(bytes_written, total_bytes);
 }
 
-int main(int argc, char* argv[]) {
-  const auto now = std::chrono::high_resolution_clock::now;
-  bool use_hermes = true;
+static void InitParticles(const int x_dim, const int y_dim, const int z_dim,
+                          std::vector<int> &id1, std::vector<int> &id2,
+                          std::vector<float> &x, std::vector<float> &y,
+                          std::vector<float> &z, std::vector<float> &px,
+                          std::vector<float> &py, std::vector<float> &pz) {
+  size_t num_particles = id1.size();
 
-  size_t num_particles = (8 * 1024 * 1024) / sizeof(float);
-  if (argc == 2) {
-    num_particles = (std::stoul(argv[2])) * 1024UL * 1024UL;
+  std::iota(id1.begin(), id1.end(), 0);
+
+  for (size_t i = 0; i < num_particles; ++i) {
+    id2[i] = id1[i] * 2;
   }
+
+  std::generate(x.begin(), x.end(), [&x_dim]() {
+    return uniform_random_number() * x_dim;
+  });
+
+  std::generate(y.begin(), y.end(), [&y_dim]() {
+    return uniform_random_number() * y_dim;
+  });
+
+  for (size_t i = 0; i < num_particles; ++i) {
+    z[i] = ((double)id1[i] / num_particles) * z_dim;
+  }
+
+  std::generate(px.begin(), px.end(), [&x_dim]() {
+    return uniform_random_number() * x_dim;
+  });
+
+  std::generate(py.begin(), py.end(), [&y_dim]() {
+    return uniform_random_number() * y_dim;
+  });
+
+  for (size_t i = 0; i < num_particles; ++i) {
+    pz[i] = ((double)id2[i] / num_particles) * z_dim;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  const int kNumVariables = 8;
+  const int kXdim = 64;
+  const int kYdim = 64;
+  const int kZdim = 64;
+  const auto now = std::chrono::high_resolution_clock::now;
+
+  Options options = HandleArgs(argc, argv);
 
   int mpi_threads_provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &mpi_threads_provided);
@@ -57,114 +163,86 @@ int main(int argc, char* argv[]) {
 
   if (hermes->IsApplicationCore()) {
     int my_rank = hermes->GetProcessRank();
+    int app_size = hermes->GetNumProcesses();
 
-    if (my_rank == 0) {
-      printf("Number of particles: %zu \n", num_particles);
+    if (options.num_buckets != 1 || options.num_buckets != kNumVariables ||
+        options.num_buckets != app_size) {
+
     }
 
-    const int kXdim = 64;
-    const int kYdim = 64;
-    const int kZdim = 64;
-
-    // Init particles
-    std::vector<int> id1(num_particles);
-    std::iota(id1.begin(), id1.end(), 0);
-
-    std::vector<int> id2(num_particles);
-    for (size_t i = 0; i < num_particles; ++i) {
-      id2[i] = id1[i] * 2;
-    }
-
-    std::vector<float> x(num_particles);
-    std::generate(x.begin(), x.end(), [&kXdim]() {
-      return uniform_random_number() * kXdim;
-    });
-
-    std::vector<float> y(num_particles);
-    std::generate(y.begin(), y.end(), [&kYdim]() {
-      return uniform_random_number() * kYdim;
-    });
-
-    std::vector<float> z(num_particles);
-    for (size_t i = 0; i < num_particles; ++i) {
-      z[i] = ((double)id1[i] / num_particles) * kZdim;
-    }
-
-    std::vector<float> px(num_particles);
-    std::generate(px.begin(), px.end(), [&kXdim]() {
-      return uniform_random_number() * kXdim;
-    });
-
-    std::vector<float> py(num_particles);
-    std::generate(py.begin(), py.end(), [&kYdim]() {
-      return uniform_random_number() * kYdim;
-    });
-
-    std::vector<float> pz(num_particles);
-    for (size_t i = 0; i < num_particles; ++i) {
-      pz[i] = ((double)id2[i] / num_particles) * kZdim;
-    }
-
-    hermes->AppBarrier();
-    // MasterPrintf(my_rank, "Finished initializing particles \n");
-
-    auto start = now();
+    std::vector<int> id1(options.num_particles);
+    std::vector<int> id2(options.num_particles);
+    std::vector<float> x(options.num_particles);
+    std::vector<float> y(options.num_particles);
+    std::vector<float> z(options.num_particles);
+    std::vector<float> px(options.num_particles);
+    std::vector<float> py(options.num_particles);
+    std::vector<float> pz(options.num_particles);
+    InitParticles(kXdim, kYdim, kZdim, id1, id2, x, y, z, px, py, pz);
 
     std::string bucket_name = "vpic_" + std::to_string(my_rank);
     std::string output_file = bucket_name + ".out";
-    hapi::Bucket bucket(bucket_name, hermes);
 
-    hermes->AppBarrier();
-    // MasterPrintf(my_rank, "Before writing particles \n");
-
-    auto start1 = now();
-
-    FILE *f = NULL;
-
-    if (use_hermes) {
-      CHECK(bucket.Put("x", x).Succeeded());
-      CHECK(bucket.Put("y", y).Succeeded());
-      CHECK(bucket.Put("z", z).Succeeded());
-      CHECK(bucket.Put("id1", id1).Succeeded());
-      CHECK(bucket.Put("id2", id2).Succeeded());
-      CHECK(bucket.Put("px", px).Succeeded());
-      CHECK(bucket.Put("py", py).Succeeded());
-      CHECK(bucket.Put("pz", pz).Succeeded());
-    } else {
-      f = fopen(output_file.c_str(), "w");
-      CHECK(f);
-      DoFwrite(x, f);
-      DoFwrite(y, f);
-      DoFwrite(z, f);
-      DoFwrite(id1, f);
-      DoFwrite(id2, f);
-      DoFwrite(px, f);
-      DoFwrite(py, f);
-      DoFwrite(pz, f);
+    std::vector<hapi::Bucket> buckets;
+    for (int i = 0; i < options.num_buckets; ++i) {
+      buckets.emplace_back(bucket_name, hermes);
     }
-
     hermes->AppBarrier();
-    auto end1 = now();
-    // MasterPrintf(my_rank, "After writing particles \n");
 
-    auto start2 = now();
-    if (use_hermes) {
-      // bucket.Persist(bucket_name + ".out");
-    } else {
-      CHECK_EQ(fflush(f), 0);
-      CHECK_EQ(fsync(fileno(f)), 0);
-      CHECK_EQ(fclose(f), 0);
+    auto start = now();
+
+    for (int i = 0; i < options.num_iterations; ++i) {
+      FILE *f = NULL;
+
+      auto start1 = now();
+      if (options.do_posix_io) {
+        f = fopen(output_file.c_str(), "w");
+        CHECK(f);
+        DoFwrite(x, f);
+        DoFwrite(y, f);
+        DoFwrite(z, f);
+        DoFwrite(id1, f);
+        DoFwrite(id2, f);
+        DoFwrite(px, f);
+        DoFwrite(py, f);
+        DoFwrite(pz, f);
+      } else {
+        CHECK(bucket.Put("x", x).Succeeded());
+        CHECK(bucket.Put("y", y).Succeeded());
+        CHECK(bucket.Put("z", z).Succeeded());
+        CHECK(bucket.Put("id1", id1).Succeeded());
+        CHECK(bucket.Put("id2", id2).Succeeded());
+        CHECK(bucket.Put("px", px).Succeeded());
+        CHECK(bucket.Put("py", py).Succeeded());
+        CHECK(bucket.Put("pz", pz).Succeeded());
+      }
+      auto end1 = now();
+
+      auto start2 = now();
+      if (options.do_posix_io) {
+        CHECK_EQ(fflush(f), 0);
+        CHECK_EQ(fsync(fileno(f)), 0);
+        CHECK_EQ(fclose(f), 0);
+      } else {
+        CHECK(bucket.DeleteBlob("x").Succeeded());
+        CHECK(bucket.DeleteBlob("y").Succeeded());
+        CHECK(bucket.DeleteBlob("z").Succeeded());
+        CHECK(bucket.DeleteBlob("px").Succeeded());
+        CHECK(bucket.DeleteBlob("py").Succeeded());
+        CHECK(bucket.DeleteBlob("pz").Succeeded());
+        CHECK(bucket.DeleteBlob("id1").Succeeded());
+        CHECK(bucket.DeleteBlob("id2").Succeeded());
+      }
+      auto end2 = now();
+
+      hermes->AppBarrier();
+
     }
-    auto end2 = now();
-
-    CHECK(bucket.Destroy().Succeeded());
-
-    hermes->AppBarrier();
 
     auto end = now();
 
     if (my_rank == 0) {
-      printf("Timing results:\n");
+      printf("num_buckets,num_iterations,num_particles\n");
       double total_seconds_writing =
         std::chrono::duration<double>(end1 - start1).count();
       printf("Seconds spent writing: %f\n", total_seconds_writing);
