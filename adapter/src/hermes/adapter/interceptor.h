8451f02a310f471aa255d6df3523e4d367e95aa6
Hariharan Devarajan <hariharandev1@llnl.gov>
2021-08-27 15:54:23 -0700

    fixed indentation.

--- adapter/src/hermes/adapter/interceptor.h
+++ adapter/src/hermes/adapter/interceptor.h
@@ -51,7 +51,8 @@ namespace hermes::adapter {
  */
 const char* kPathExclusions[] = {"/bin/", "/boot/", "/dev/",  "/etc/",
                                  "/lib/", "/opt/",  "/proc/", "/sbin/",
-                                 "/sys/", "/usr/",  "/var/",  "/run/", "pipe"};
+                                 "/sys/", "/usr/",  "/var/",  "/run/",
+                                 "pipe"};
 /**
  * Paths prefixed with the following directories are tracked by Hermes even if
  * they share a root with a path listed in path_exclusions
