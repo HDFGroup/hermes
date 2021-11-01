d33caa96d354e440017bc3ee2bf4b4cf7b99be55
Kimmy Mu <kmu@hdfgroup.org>
2021-11-01 09:43:42 -0500

    initialize glog properly

--- src/api/hermes.cc
+++ src/api/hermes.cc
@@ -11,11 +11,13 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <sys/mman.h>
+#include <unistd.h>
 
 #include <cmath>
 
 #include "glog/logging.h"
 
+#include "hermes_config.h"
 #include "utils.h"
 #include "hermes.h"
 #include "bucket.h"
@@ -239,8 +241,43 @@ BootstrapSharedMemory(Arena *arenas, Config *config, CommunicationContext *comm,
   return result;
 }
 
+static std::string getexe() {
+  char result[PATH_MAX];
+  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+  return std::string(result, (count > 0) ? count : 0);
+}
+
+static void InitGlog() {
+#ifdef GLOG_TO_CONSOLE
+  FLAGS_logtostderr = 1;
+#else
+  // TODO(kimmy): log path from user
+  char cwd[PATH_MAX];
+  FLAGS_log_dir = getcwd(cwd, sizeof(cwd));
+#endif
+
+#ifdef HERMES_GLOG_SEVERITY_LEVEL
+  FLAGS_minloglevel = HERMES_GLOG_SEVERITY_LEVEL;
+#else
+  FLAGS_minloglevel = 0;
+#endif
+
+#ifdef HERMES_GLOG_VERBOSE_LEVEL
+  FLAGS_v = HERMES_GLOG_VERBOSE_LEVEL;
+#else
+  FLAGS_v = 0;
+#endif
+
+  if (getexe().empty())
+    google::InitGoogleLogging("hermes");
+  else
+    google::InitGoogleLogging(getexe().c_str());
+}
+
 std::shared_ptr<api::Hermes> InitHermes(Config *config, bool is_daemon,
                                         bool is_adapter) {
+  InitGlog();
+
   std::string base_shmem_name(config->buffer_pool_shmem_name);
   MakeFullShmemName(config->buffer_pool_shmem_name, base_shmem_name.c_str());
 
