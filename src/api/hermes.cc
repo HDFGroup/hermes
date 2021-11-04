4d95f234f29623c2f3cb3270a48d8b1e34670def
Kimmy Mu <kmu@hdfgroup.org>
2021-11-04 08:05:32 -0500

    initialize glog with simple version

--- src/api/hermes.cc
+++ src/api/hermes.cc
@@ -236,8 +236,18 @@ BootstrapSharedMemory(Arena *arenas, Config *config, CommunicationContext *comm,
   return result;
 }
 
+static void InitGlog() {
+  FLAGS_logtostderr = 1;
+  FLAGS_minloglevel = 0;
+  FLAGS_v = 0;
+
+  google::InitGoogleLogging("hermes");
+}
+
 std::shared_ptr<api::Hermes> InitHermes(Config *config, bool is_daemon,
                                         bool is_adapter) {
+  InitGlog();
+
   std::string base_shmem_name(config->buffer_pool_shmem_name);
   MakeFullShmemName(config->buffer_pool_shmem_name, base_shmem_name.c_str());
 
