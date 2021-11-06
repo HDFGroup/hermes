f6f7dbf525fe1c2579e083a5f96daae0761a2e0a
Gerd Heber <gheber@hdfgroup.org>
2021-11-05 19:24:03 -0500

    Linter happy?

--- src/hermes_types.h
+++ src/hermes_types.h
@@ -28,31 +28,31 @@
 
 namespace hermes {
 
-  typedef uint8_t u8;
-  typedef uint16_t u16;
-  typedef uint32_t u32;
-  typedef uint64_t u64;
-  typedef int8_t i8;
-  typedef int16_t i16;
-  typedef int32_t i32;
-  typedef int64_t i64;
-  typedef float f32;
-  typedef double f64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
 
-  typedef u16 DeviceID;
+typedef u16 DeviceID;
 
 namespace api {
-  typedef std::vector<unsigned char> Blob;
+typedef std::vector<unsigned char> Blob;
 
-  /** Supported data placement policies */
-  enum class PlacementPolicy {
+/** Supported data placement policies */
+enum class PlacementPolicy {
     kRandom,          /**< Random blob placement */
     kRoundRobin,      /**< Round-Robin (around devices) blob placement */
     kMinimizeIoTime,  /**< LP-based blob placement, minimize I/O time */
-  };
+};
 
-  /** Hermes API call context */
-  struct Context {
+/** Hermes API call context */
+struct Context {
     static int default_buffer_organizer_retries;
     /**< The default maximum number of buffer organizer retries */
 
@@ -61,7 +61,7 @@ namespace api {
 
     static bool default_rr_split;
     /**< Whether random splitting of blobs is enabled for Round-Robin blob
-         placement. */
+       placement. */
 
     PlacementPolicy policy;
     /**< The blob placement policy */
@@ -83,79 +83,79 @@ namespace api {
                 rr_split(default_rr_split),
                 rr_retry(false),
                 disable_swap(false) {}
-    };
+};
 
-  }  // namespace api
+}  // namespace api
 
-  // TODO(chogan): These constants impose limits on the number of slabs,
-  // devices, file path lengths, and shared memory name lengths, but eventually
-  // we should allow arbitrary sizes of each.
-  static constexpr int kMaxBufferPoolSlabs = 8;
-  constexpr int kMaxPathLength = 256;
-  constexpr int kMaxBufferPoolShmemNameLength = 64;
-  constexpr int kMaxDevices = 8;
-  constexpr int kMaxBucketNameSize = 256;
-  constexpr int kMaxVBucketNameSize = 256;
+// TODO(chogan): These constants impose limits on the number of slabs,
+// devices, file path lengths, and shared memory name lengths, but eventually
+// we should allow arbitrary sizes of each.
+static constexpr int kMaxBufferPoolSlabs = 8;
+constexpr int kMaxPathLength = 256;
+constexpr int kMaxBufferPoolShmemNameLength = 64;
+constexpr int kMaxDevices = 8;
+constexpr int kMaxBucketNameSize = 256;
+constexpr int kMaxVBucketNameSize = 256;
 
-  constexpr char kPlaceInHierarchy[] = "PlaceInHierarchy";
+constexpr char kPlaceInHierarchy[] = "PlaceInHierarchy";
 
 #define HERMES_NOT_IMPLEMENTED_YET                      \
-  LOG(FATAL) << __func__ << " not implemented yet\n"
+    LOG(FATAL) << __func__ << " not implemented yet\n"
 
 #define HERMES_INVALID_CODE_PATH                    \
-  LOG(FATAL) << "Invalid code path." << std::endl
+    LOG(FATAL) << "Invalid code path." << std::endl
 
-  /** A TargetID uniquely identifies a buffering target within the system. */
-  union TargetID {
+/** A TargetID uniquely identifies a buffering target within the system. */
+union TargetID {
     /** The Target ID as bitfield */
     struct {
-      /** The ID of the node in charge of this target. */
-      u32 node_id;
-      /** The ID of the virtual device that backs this target. It is an index into
-       * the Device array starting at BufferPool::devices_offset (on the node with
-       * ID node_id). */
-      u16 device_id;
-      /** The index into the Target array starting at BufferPool::targets_offset
-       * (on the node with ID node_id). */
-      u16 index;
+        /** The ID of the node in charge of this target. */
+        u32 node_id;
+        /** The ID of the virtual device that backs this target. It is an index into
+         * the Device array starting at BufferPool::devices_offset (on the node with
+         * ID node_id). */
+        u16 device_id;
+        /** The index into the Target array starting at BufferPool::targets_offset
+         * (on the node with ID node_id). */
+        u16 index;
     } bits;
 
     /** The TargetID as a unsigned 64-bit integer */
     u64 as_int;
-  };
+};
 
-  /**
-   * A PlacementSchema is a vector of (size, target) pairs where size is the
-   * number of bytes to buffer and target is the TargetID where to buffer those
-   * bytes.
-   */
-  using PlacementSchema = std::vector<std::pair<size_t, TargetID>>;
+/**
+ * A PlacementSchema is a vector of (size, target) pairs where size is the
+ * number of bytes to buffer and target is the TargetID where to buffer those
+ * bytes.
+ */
+using PlacementSchema = std::vector<std::pair<size_t, TargetID>>;
 
-  /**
-   * Distinguishes whether the process (or rank) is part of the application cores
-   * or the Hermes core(s).
-   */
-  enum class ProcessKind {
+/**
+ * Distinguishes whether the process (or rank) is part of the application cores
+ * or the Hermes core(s).
+ */
+enum class ProcessKind {
     kApp,     /**< Application process */
     kHermes,  /**< Hermes core process */
 
     kCount    /**< Sentinel value */
-  };
+};
 
-  /** Arena types */
-  enum ArenaType {
+/** Arena types */
+enum ArenaType {
     kArenaType_BufferPool,      /**< Buffer pool: This must always be first! */
     kArenaType_MetaData,        /**< Metadata                                */
     kArenaType_Transient,       /**< Scratch space                           */
     kArenaType_TransferWindow,  /**< Data transfer                           */
 
     kArenaType_Count            /**< Sentinel value                          */
-  };
+};
 
-  /**
-   * System and user configuration that is used to initialize Hermes.
-   */
-  struct Config {
+/**
+ * System and user configuration that is used to initialize Hermes.
+ */
+struct Config {
     /** The total capacity of each buffering Device */
     size_t capacities[kMaxDevices];
     /** The block sizes of each Device */
@@ -194,11 +194,11 @@ namespace api {
     std::string mount_points[kMaxDevices];
     /** The mount point of the swap target. */
     std::string swap_mount;
-    /** The number of times the BufferOrganizer will attempt to place a swap blob
-     * into the hierarchy before giving up.*/
+    /** The number of times the BufferOrganizer will attempt to place a swap
+     * blob into the hierarchy before giving up. */
     int num_buffer_organizer_retries;
 
-    /** If non-zero, the device is shared among all nodes (e.g., burst buffers)*/
+    /** If non-zero, the device is shared among all nodes (e.g., burst buffs) */
     int is_shared_device[kMaxDevices];
 
     /** The hostname of the RPC server, minus any numbers that Hermes may
@@ -230,75 +230,75 @@ namespace api {
      * value of the USER environment variable to this string.
      */
     char buffer_pool_shmem_name[kMaxBufferPoolShmemNameLength];
-  };
+};
 
-  union BucketID {
+union BucketID {
     /** The Bucket ID as bitfield */
     struct {
-      /** The index into the Target array starting at BufferPool::targets_offset
-       * (on the node with ID node_id). */
-      u32 index;
-      /** The ID of the node in charge of this bucket. */
-      u32 node_id;
+        /** The index into the Target array starting at BufferPool::targets_offset
+         * (on the node with ID node_id). */
+        u32 index;
+        /** The ID of the node in charge of this bucket. */
+        u32 node_id;
     } bits;
 
     /** The BucketID as a unsigned 64-bit integer */
     u64 as_int;
-  };
+};
 
-  // NOTE(chogan): We reserve sizeof(BucketID) * 2 bytes in order to embed the
-  // BucketID into the Blob name. See MakeInternalBlobName() for a description of
-  // why we need double the bytes of a BucketID.
-  constexpr int kBucketIdStringSize = sizeof(BucketID) * 2;
-  constexpr int kMaxBlobNameSize = 64 - kBucketIdStringSize;
+// NOTE(chogan): We reserve sizeof(BucketID) * 2 bytes in order to embed the
+// BucketID into the Blob name. See MakeInternalBlobName() for a description of
+// why we need double the bytes of a BucketID.
+constexpr int kBucketIdStringSize = sizeof(BucketID) * 2;
+constexpr int kMaxBlobNameSize = 64 - kBucketIdStringSize;
 
-  union VBucketID {
+union VBucketID {
     /** The VBucket ID as bitfield */
     struct {
-      /** The index into the Target array starting at BufferPool::targets_offset
-       * (on the node with ID node_id). */
-      u32 index;
-      /** The ID of the node in charge of this vbucket. */
-      u32 node_id;
+        /** The index into the Target array starting at BufferPool::targets_offset
+         * (on the node with ID node_id). */
+        u32 index;
+        /** The ID of the node in charge of this vbucket. */
+        u32 node_id;
     } bits;
 
     /** The VBucketID as a unsigned 64-bit integer */
     u64 as_int;
-  };
+};
 
-  union BlobID {
+union BlobID {
     /** The Blob ID as bitfield */
     struct {
-      /** The index into the Target array starting at BufferPool::targets_offset
-       * (on the node with ID node_id). */
-      u32 buffer_ids_offset;
-      /** The ID of the node in charge of this bucket. (Negative when in swap
-          space.) */
-      i32 node_id;
+        /** The index into the Target array starting at BufferPool::targets_offset
+         * (on the node with ID node_id). */
+        u32 buffer_ids_offset;
+        /** The ID of the node in charge of this bucket. (Negative when in swap
+            space.) */
+        i32 node_id;
     } bits;
 
     /** The BlobID as a unsigned 64-bit integer */
     u64 as_int;
-  };
+};
 
-  /** Trait ID type */
-  typedef u64 TraitID;
+/** Trait ID type */
+typedef u64 TraitID;
 
-  namespace api {
+namespace api {
 
-    /** Trait types */
-    enum class TraitType : u8 {
-      META = 0,
-      DATA = 1,
-      FILE_MAPPING = 2,
-      PERSIST = 3,
-    };
-  }  // namespace api
+/** Trait types */
+enum class TraitType : u8 {
+    META = 0,
+    DATA = 1,
+    FILE_MAPPING = 2,
+    PERSIST = 3,
+};
+}  // namespace api
 
-  struct TraitIdArray {
+struct TraitIdArray {
     TraitID *ids;
     u32 length;
-  };
+};
 
 
 
