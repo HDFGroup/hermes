1d4dbffe72b4d2fcc68275c0b92ad7ce3eb87b56
jaime <jcernudagarcia@hawk.iit.edu>
2022-01-07 21:13:20 -0600

    Fixing tests - Fixing headers on tests - Adding licences to the tests

--- adapter/test/pubsub/pubsub_metadata_test.cc
+++ adapter/test/pubsub/pubsub_metadata_test.cc
@@ -1,8 +1,17 @@
-//
-// Created by jaime on 6/25/2021.
-//
-#include <hermes/adapter/pubsub/metadata_manager.h>
-#include <hermes/adapter/singleton.h>
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <metadata_manager.h>
+#include <singleton.h>
 
 int main(int argc, char **argv) {
   auto mdm = hermes::adapter::Singleton<hermes::adapter::pubsub::MetadataManager>::GetInstance(false);
