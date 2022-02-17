67458699e1d704e77f8840f8da2577caaaf40357
Chris Hogan <c096h800@gmail.com>
2022-02-17 10:15:42 -0600

    Remove ReadOnlyTrait

--- test/buffer_organizer_test.cc
+++ test/buffer_organizer_test.cc
@@ -267,39 +267,6 @@ static void TestWriteOnlyBucket() {
   hermes->Finalize(true);
 }
 
-static void TestReadOnlyBucket() {
-  HermesPtr hermes = hermes::InitHermesDaemon();
-  std::string bkt_name = "ReadOnly";
-  VBucket vbkt(bkt_name, hermes);
-  Bucket bkt(bkt_name, hermes);
-
-  hapi::ReadOnlyTrait trait;
-  vbkt.Attach(&trait);
-
-  const int kBlobSize = KILOBYTES(4);
-  const u8 kBlobData = 127;
-  hapi::Blob blob(kBlobSize, kBlobData);
-
-  const int kIters = 128;
-  for (int i = 0; i < kIters; ++i) {
-    std::string blob_name = "b" + std::to_string(i);
-    bkt.Put(blob_name, blob);
-    vbkt.Link(blob_name, bkt_name);
-  }
-
-  for (int i = 0; i < kIters; ++i) {
-    std::string blob_name = "b" + std::to_string(i);
-    hapi::Blob retrieved_data(kBlobSize);
-    // Call Get through VBucket so the OnGet callback is triggered
-    vbkt.Get(blob_name, bkt, retrieved_data);
-    Assert(retrieved_data == blob);
-  }
-
-  vbkt.Destroy();
-  bkt.Destroy();
-  hermes->Finalize(true);
-}
-
 int main(int argc, char *argv[]) {
   int mpi_threads_provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &mpi_threads_provided);
@@ -319,7 +286,6 @@ int main(int argc, char *argv[]) {
   HERMES_ADD_TEST(TestBoMove);
   HERMES_ADD_TEST(TestOrganizeBlob);
   HERMES_ADD_TEST(TestWriteOnlyBucket);
-  HERMES_ADD_TEST(TestReadOnlyBucket);
 
 #undef HERMES_ADD_TEST
 
