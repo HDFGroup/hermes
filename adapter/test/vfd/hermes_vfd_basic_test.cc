7e3b5f30e44ef98bf17911ffe394a2c8c358fd97
Chris Hogan <c096h800@gmail.com>
2022-04-12 16:36:18 -0500

    WIP: Tests working

--- adapter/test/vfd/hermes_vfd_basic_test.cc
+++ adapter/test/vfd/hermes_vfd_basic_test.cc
@@ -10,45 +10,43 @@
  * have access to the file, you may request a copy from help@hdfgroup.org.   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-using hermes::adapter::vfd::test::VfdApi;
 using hermes::adapter::vfd::test::MuteHdf5Errors;
-using hermes::adapter::vfd::test::AssertFunc;
 
-TEST_CASE("Open", "[process=" + std::to_string(info.comm_size) +
+TEST_CASE("H5FOpen", "[process=" + std::to_string(info.comm_size) +
                       "]"
                       "[operation=single_open]"
                       "[repetition=1][file=1]") {
-  pretest();
+  Pretest();
   SECTION("open non-existent file") {
     MuteHdf5Errors mute;
-    test::test_open(info.new_file, H5F_ACC_RDONLY);
+    test::TestOpen(info.new_file, H5F_ACC_RDONLY);
     REQUIRE(test::hermes_hid == H5I_INVALID_HID);
-    test::test_open(info.new_file, H5F_ACC_RDWR);
+    test::TestOpen(info.new_file, H5F_ACC_RDWR);
     REQUIRE(test::hermes_hid == H5I_INVALID_HID);
   }
 
   SECTION("truncate existing file") {
     bool create = true;
-    test::test_open(info.existing_file, H5F_ACC_TRUNC, create);
+    test::TestOpen(info.existing_file, H5F_ACC_TRUNC, create);
     REQUIRE(test::hermes_hid != H5I_INVALID_HID);
-    test::test_close();
+    test::TestClose();
     REQUIRE(test::hermes_herr >= 0);
   }
 
   SECTION("open existing file") {
     {
-      test::test_open(info.existing_file, H5F_ACC_RDWR);
+      test::TestOpen(info.existing_file, H5F_ACC_RDWR);
       REQUIRE(test::hermes_hid != H5I_INVALID_HID);
-      test::test_close();
+      test::TestClose();
       REQUIRE(test::hermes_herr >= 0);
     }
 
     {
-      test::test_open(info.existing_file, H5F_ACC_RDONLY);
+      test::TestOpen(info.existing_file, H5F_ACC_RDONLY);
       REQUIRE(test::hermes_hid != H5I_INVALID_HID);
-      test::test_close();
+      test::TestClose();
       REQUIRE(test::hermes_herr >= 0);
     }
   }
-  posttest();
+  Posttest();
 }
