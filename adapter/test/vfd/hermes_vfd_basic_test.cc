330ed4ab3345f957a725e7a869751520973b06db
Chris Hogan <c096h800@gmail.com>
2022-04-13 16:07:02 -0500

    WIP: Adding VFD tests

--- adapter/test/vfd/hermes_vfd_basic_test.cc
+++ adapter/test/vfd/hermes_vfd_basic_test.cc
@@ -34,19 +34,100 @@ TEST_CASE("H5FOpen", "[process=" + std::to_string(info.comm_size) +
   }
 
   SECTION("open existing file") {
-    {
-      test::TestOpen(info.existing_file, H5F_ACC_RDWR);
-      REQUIRE(test::hermes_hid != H5I_INVALID_HID);
-      test::TestClose();
-      REQUIRE(test::hermes_herr >= 0);
-    }
+    test::TestOpen(info.existing_file, H5F_ACC_RDWR);
+    REQUIRE(test::hermes_hid != H5I_INVALID_HID);
+    test::TestClose();
+    REQUIRE(test::hermes_herr >= 0);
 
-    {
-      test::TestOpen(info.existing_file, H5F_ACC_RDONLY);
-      REQUIRE(test::hermes_hid != H5I_INVALID_HID);
-      test::TestClose();
-      REQUIRE(test::hermes_herr >= 0);
-    }
+    test::TestOpen(info.existing_file, H5F_ACC_RDONLY);
+    REQUIRE(test::hermes_hid != H5I_INVALID_HID);
+    test::TestClose();
+    REQUIRE(test::hermes_herr >= 0);
   }
+
+  SECTION("create existing file exclusively") {
+    MuteHdf5Errors mute;
+    bool create = true;
+    test::TestOpen(info.existing_file, H5F_ACC_EXCL, create);
+    REQUIRE(test::hermes_hid == H5I_INVALID_HID);
+  }
+
+  Posttest();
+}
+
+TEST_CASE("SingleWrite", "[process=" + std::to_string(info.comm_size) +
+                             "]"
+                             "[operation=single_write]"
+                             "[request_size=type-fixed][repetition=1]"
+                             "[file=1]") {
+  Pretest();
+
+  // SECTION("write to existing file") {
+  //   test::TestOpen(info.existing_file, H5F_ACC_RDWR);
+  //   REQUIRE(test::hermes_hid != H5I_INVALID_HID);
+  //   test::test_seek(0, SEEK_SET);
+  //   REQUIRE(test::status_orig == 0);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  // }
+
+  // SECTION("write to new file") {
+  //   test::TestOpen(info.new_file, O_WRONLY | O_CREAT | O_EXCL, 0600);
+  //   REQUIRE(test::fh_orig != -1);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  //   REQUIRE(fs::file_size(info.new_file) == test::size_written_orig);
+  // }
+
+  // SECTION("write to existing file with truncate") {
+  //   test::TestOpen(info.existing_file, O_WRONLY | O_TRUNC);
+  //   REQUIRE(test::fh_orig != -1);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  //   REQUIRE(fs::file_size(info.existing_file) == test::size_written_orig);
+  // }
+
+  // SECTION("write to existing file at the end") {
+  //   test::TestOpen(info.existing_file, O_RDWR);
+  //   REQUIRE(test::fh_orig != -1);
+  //   test::test_seek(0, SEEK_END);
+  //   REQUIRE(((size_t)test::status_orig) ==
+  //           args.request_size * info.num_iterations);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  //   REQUIRE(fs::file_size(info.existing_file) ==
+  //           test::size_written_orig + args.request_size * info.num_iterations);
+  // }
+
+  // SECTION("append to existing file") {
+  //   auto existing_size = fs::file_size(info.existing_file);
+  //   test::TestOpen(info.existing_file, O_RDWR | O_APPEND);
+  //   REQUIRE(test::fh_orig != -1);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  //   REQUIRE(fs::file_size(info.existing_file) ==
+  //           existing_size + test::size_written_orig);
+  // }
+
+  // SECTION("append to new file") {
+  //   test::TestOpen(info.new_file, O_WRONLY | O_CREAT | O_EXCL, 0600);
+  //   REQUIRE(test::fh_orig != -1);
+  //   test::test_write(info.write_data.data(), args.request_size);
+  //   REQUIRE(test::size_written_orig == args.request_size);
+  //   test::TestClose();
+  //   REQUIRE(test::status_orig == 0);
+  //   REQUIRE(fs::file_size(info.new_file) == test::size_written_orig);
+  // }
+
   Posttest();
 }
