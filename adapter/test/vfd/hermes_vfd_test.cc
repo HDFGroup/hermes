8285c0eb898920f72c87a27c3be027b9021f29f0
Chris Hogan <c096h800@gmail.com>
2022-04-26 09:38:29 -0500

    Document VFD test functions and structs.

--- adapter/test/vfd/hermes_vfd_test.cc
+++ adapter/test/vfd/hermes_vfd_test.cc
@@ -34,7 +34,6 @@ namespace hermes::adapter::vfd::test {
 
 struct Arguments {
   std::string filename = "test";
-  std::string extension = ".h5";
   std::string directory = "/tmp";
   size_t request_size = 65536;
 };
@@ -50,6 +49,7 @@ struct TestInfo {
   std::string existing_file;
   std::string new_file_cmp;
   std::string existing_file_cmp;
+  std::string hdf5_extension = ".h5";
   size_t num_iterations = 64;
   int offset_seed = 1;
   unsigned int rs_seed = 1;
@@ -67,6 +67,12 @@ struct TestInfo {
   bool scratch_mode = false;
 };
 
+/**
+ * Temporarily disable printing of the HDF5 error stack.
+ *
+ * Some tests intentionally trigger HDF5 errors, and in those cases we don't
+ * want to clutter the output with HDF5 error messages.
+ */
 class MuteHdf5Errors {
   H5E_auto2_t old_func;
   void *old_client_data;
@@ -86,15 +92,26 @@ class MuteHdf5Errors {
   }
 };
 
+/**
+ * HDF5 identifiers required for reads and writes.
+ */
 struct RwIds {
   hid_t dset_id;
   hid_t dspace_id;
   hid_t mspace_id;
 };
 
-// TODO(chogan): Document
-// Working towards a common adapter api that adapters can subclass
+/**
+ * The I/O API for this adapter layer.
+ *
+ * Ideally we would have a high level Adapter I/O API that each adapter inherits
+ * from so that the adapter tests can reuse more code. This is a step in that
+ * direction.
+ */
 struct Hdf5Api {
+  /**
+   * A file access property list representing the sec2 (POSIX) VFD.
+   */
   hid_t sec2_fapl;
 
   Hdf5Api() : sec2_fapl(H5I_INVALID_HID) {
@@ -108,30 +125,46 @@ struct Hdf5Api {
     sec2_fapl = H5I_INVALID_HID;
   }
 
+  /**
+   * Open an existing file using the default VFD. Since the tests are run with
+   * HDF5_DRIVER=hermes, the default VFD will be the Hermes VFD.
+   */
   hid_t Open(const std::string &fname, unsigned flags) {
     hid_t result = H5Fopen(fname.c_str(), flags, H5P_DEFAULT);
 
     return result;
   }
 
+  /**
+   * Open an existing file using the POSIX VFD. This will bypass Hermes.
+   */
   hid_t OpenPosix(const std::string &fname, unsigned flags) {
     hid_t result = H5Fopen(fname.c_str(), flags, sec2_fapl);
 
     return result;
   }
 
+  /**
+   * Create a file using the default VFD.
+   */
   hid_t Create(const std::string &fname, unsigned flags) {
     hid_t result = H5Fcreate(fname.c_str(), flags, H5P_DEFAULT, H5P_DEFAULT);
 
     return result;
   }
 
+  /**
+   * Create a file using the POSIX VFD.
+   */
   hid_t CreatePosix(const std::string &fname, unsigned flags) {
     hid_t result = H5Fcreate(fname.c_str(), flags, H5P_DEFAULT, sec2_fapl);
 
     return result;
   }
 
+  /**
+   * Boilerplate necessary before calling H5Dread or H5Dwrite.
+   */
   RwIds RwPreamble(hid_t hid, const std::string &dset_name, hsize_t offset,
                    hsize_t nelems, hsize_t stride = 1) {
     hid_t dset_id = H5Dopen2(hid, dset_name.c_str(), H5P_DEFAULT);
@@ -156,12 +189,19 @@ struct Hdf5Api {
     return result;
   }
 
+  /**
+   * Cleanup code required after H5Dread and H5Dwrite.
+   */
   void RwCleanup(RwIds *ids) {
     REQUIRE(H5Sclose(ids->mspace_id) >= 0);
     REQUIRE(H5Sclose(ids->dspace_id) >= 0);
     REQUIRE(H5Dclose(ids->dset_id) >= 0);
   }
 
+  /**
+   * Reads @p nelems elements from the object represented by @p hid into @p buf,
+   * starting at element @p offset.
+   */
   void Read(hid_t hid, const std::string &dset_name, std::vector<f32> &buf,
             hsize_t offset, hsize_t nelems) {
     RwIds ids = RwPreamble(hid, dset_name, offset, nelems);
@@ -177,6 +217,10 @@ struct Hdf5Api {
     MakeDataset(hid, dset_name, data.data(), data.size(), compact);
   }
 
+  /**
+   * Create a 1-dimensional dataset named @pdset_name in object @p hid with @p
+   * nelems elements from the array @p data.
+   */
   void MakeDataset(hid_t hid, const std::string &dset_name, const f32 *data,
                    hsize_t nelems, bool compact = false) {
     hid_t dcpl = H5P_DEFAULT;
@@ -212,6 +256,11 @@ struct Hdf5Api {
     }
   }
 
+  /**
+   * Write @p nelems elements to the dataset @p dset_name in file @p hid
+   * starting at element @p offset. The dataset will be created if it doesn't
+   * already exist.
+   */
   void WritePartial1d(hid_t hid, const std::string &dset_name,
                         const f32 *data, hsize_t offset, hsize_t nelems) {
     RwIds ids = RwPreamble(hid, dset_name, offset, nelems);
@@ -229,7 +278,7 @@ struct Hdf5Api {
   }
 };
 
-// xoshiro128+ random number generation:
+// xoshiro128+ random number generation. 2x speedup over std::mt19937:
 // https://prng.di.unimi.it/xoshiro128plus.c
 
 static inline u32 RotateLeft(const u32 x, int k) {
@@ -257,6 +306,9 @@ u32 GenNextRandom() {
   return random;
 }
 
+/**
+ * Return a random float in the range [0.0f, 1.0f]
+ */
 f32 GenRandom0to1() {
   u32 random_u32 = GenNextRandom();
 
@@ -265,6 +317,10 @@ f32 GenRandom0to1() {
   return result;
 }
 
+/**
+ * Create an HDF5 file called @p fname with @p num_datasets datasets, each with
+ * @p num_dataset_elems elements.
+ */
 void GenHdf5File(std::string fname, size_t num_dataset_elems,
                  size_t num_datasets) {
   std::vector<f32> data(num_dataset_elems * num_datasets);
@@ -296,6 +352,10 @@ using hermes::adapter::vfd::test::GenHdf5File;
 using hermes::adapter::vfd::test::GenNextRandom;
 using hermes::adapter::vfd::test::GenRandom0to1;
 
+/**
+ * Called in the Catch2 main function (see catch_config.h) before any tests are
+ * run. Initialize sizes, filenames, and read/write buffers.
+ */
 int init(int* argc, char*** argv) {
   MPI_Init(argc, argv);
   if (args.request_size % info.element_size != 0) {
@@ -314,7 +374,7 @@ int init(int* argc, char*** argv) {
 
   fs::path fullpath = args.directory;
   fullpath /= args.filename;
-  std::string suffix = std::to_string(getpid()) + args.extension;
+  std::string suffix = std::to_string(getpid()) + info.hdf5_extension;
   info.new_file = fullpath.string() + "_new_" + suffix;
   info.existing_file = fullpath.string() + "_ext_" + suffix;
   info.new_file_cmp = fullpath.string() + "_new_cmp_" + suffix;
@@ -332,11 +392,17 @@ int init(int* argc, char*** argv) {
   return 0;
 }
 
+/**
+ * Called from catch_config.h after all tests are run.
+ */
 int finalize() {
   MPI_Finalize();
   return 0;
 }
 
+/**
+ * Remove all files generated by the tests
+ */
 void CleanupFiles() {
   if (fs::exists(info.new_file)) fs::remove(info.new_file);
   if (fs::exists(info.new_file_cmp)) fs::remove(info.new_file_cmp);
@@ -344,6 +410,11 @@ void CleanupFiles() {
   if (fs::exists(info.existing_file_cmp)) fs::remove(info.existing_file_cmp);
 }
 
+/**
+ * Called before each individual test.
+ *
+ * Generates files for tests that operate on existing files.
+ */
 int Pretest() {
   CleanupFiles();
 
@@ -357,6 +428,10 @@ int Pretest() {
   return 0;
 }
 
+/**
+ * Use h5diff to ensure that the resulting files from the Hermes VFD and the
+ * POSIX VFD are the same.
+ */
 void CheckResults(const std::string &file1, const std::string &file2) {
   if (fs::exists(file1) && fs::exists(file2)) {
     std::string h5diff_cmd = "h5diff " + file1 + " " + file2;
@@ -368,8 +443,14 @@ void CheckResults(const std::string &file1, const std::string &file2) {
   }
 }
 
+/**
+ * Called after each individual test.
+ */
 int Posttest() {
   if (!info.scratch_mode) {
+    // NOTE(chogan): This is necessary so that h5diff doesn't use the Hermes VFD
+    // in CheckResults. We don't need to reset LD_PRELOAD because it only has an
+    // effect when an application first starts.
     unsetenv("LD_PRELOAD");
     unsetenv("HDF5_DRIVER");
     CheckResults(info.new_file, info.new_file_cmp);
@@ -393,6 +474,14 @@ cl::Parser define_options() {
 
 using hermes::adapter::vfd::test::Hdf5Api;
 
+/**
+ * The functions in this namespace perform operations on 2 files: the "main"
+ * file, which is the one going through the Hermes VFD, and a comparison file
+ * (with a "_cmp" suffix) which goes through the POSIX VFD. The idea is to
+ * perfrom each test on 2 files (Hermes VFD and POSIX VFD) and then compare the
+ * results at the end with h5diff. In persistent mode, the file produced by the
+ * Hermes VFD should be exactly the same as the one produced by the POSIX VFD.
+ */
 namespace test {
 
 hid_t hermes_hid;
