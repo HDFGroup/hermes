f9c2ea2fd6089ade91a33f35a3f6d9fceda44942
Chris Hogan <c096h800@gmail.com>
2022-06-15 07:43:03 -0500

    Add GLOG dependency to spack package

--- ci/hermes/packages/hermes/package.py
+++ ci/hermes/packages/hermes/package.py
@@ -11,6 +11,7 @@ class Hermes(CMakePackage):
     depends_on('catch2@2.13.3')
     depends_on('glpk@4:')
     depends_on('mpich@3.3.2:')
+    depends_on('glog@0.4:')
     depends_on('hdf5@1.13.0:', when='+vfd')
 
     def cmake_args(self):
