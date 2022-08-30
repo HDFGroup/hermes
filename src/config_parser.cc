d36a9bfa11bb8bff6bd1ef13aabc1d5926febd65
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-30 10:26:09 -0500

    Make config_parser.h simple

--- src/config_parser.cc
+++ src/config_parser.cc
@@ -10,13 +10,22 @@
  * have access to the file, you may request a copy from help@hdfgroup.org.   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yaml-cpp/yaml.h>
+#include <ostream>
+#include <glog/logging.h>
+#include "utils.h"
 #include "config_parser.h"
 
-
 // Steps to add a new configuration variable:
-// 6. Add an Assert to config_parser_test.cc to test the functionality.
-// 7. Set a default value in InitDefaultConfig
-// 8. Add the variable with documentation to test/data/hermes.conf
+// 1. Add an "if" statement similar to ParseConfigYAML function for the variable
+// 2. Add an Assert to config_parser_test.cc to test the functionality.
+// 3. Set a default value in InitDefaultConfig
+// 4. Add the variable with documentation to test/data/hermes.yaml
 
 namespace hermes {
 
@@ -63,6 +72,135 @@ static const char *kConfigVariableStrings[] = {
   "default_rr_split",
   "bo_capacity_thresholds",
 };
+
+void PrintExpectedAndFail(const std::string &expected, u32 line_number = 0) {
+  std::ostringstream msg;
+  msg << "Configuration parser expected '" << expected << "'";
+  if (line_number > 0) {
+    msg << " on line " << line_number;
+  }
+  msg << "\n";
+
+  LOG(FATAL) << msg.str();
+}
+
+void RequireNumDevices(Config *config) {
+  if (config->num_devices == 0) {
+    LOG(FATAL) << "The configuration variable 'num_devices' must be defined "
+               << "first" << std::endl;
+  }
+}
+
+void RequireNumSlabs(Config *config) {
+  if (config->num_slabs == 0) {
+    LOG(FATAL) << "The configuration variable 'num_slabs' must be defined first"
+               << std::endl;
+  }
+}
+
+void RequireCapacitiesUnset(bool &already_specified) {
+  if (already_specified) {
+    LOG(FATAL) << "Capacities are specified multiple times in the configuration"
+               << " file. Only use one of 'capacities_bytes', 'capacities_kb',"
+               << "'capacities_mb', or 'capacities_gb'\n";
+  } else {
+    already_specified = true;
+  }
+}
+
+void RequireBlockSizesUnset(bool &already_specified) {
+  if (already_specified) {
+    LOG(FATAL) << "Block sizes are specified multiple times in the "
+               << "configuration file. Only use one of 'block_sizes_bytes',"
+               << "'block_sizes_kb', 'block_sizes_mb', or 'block_sizes_gb'\n";
+  } else {
+    already_specified = true;
+  }
+}
+
+void ParseCapacities(Config *config, YAML::Node capacities, int unit_conversion, bool &already_specified) {
+  int i = 0;
+  RequireNumDevices(config);
+  RequireCapacitiesUnset(already_specified);
+  for(auto val_node : capacities) {
+    config->capacities[i++] = val_node.as<size_t>() * unit_conversion;
+  }
+}
+
+void ParseBlockSizes(Config *config, YAML::Node block_sizes, int unit_conversion, bool &already_specified) {
+  int i = 0;
+  RequireNumDevices(config);
+  RequireBlockSizesUnset(already_specified);
+  for(auto val_node : block_sizes) {
+    size_t block_size = val_node.as<size_t>() * unit_conversion;
+    if (block_size > INT_MAX) {
+      LOG(FATAL) << "Max supported block size is " << INT_MAX << " bytes. "
+                 << "Config file requested " << block_size << " bytes\n";
+    }
+    config->block_sizes[i++] = block_size;
+  }
+}
+
+template<typename T>
+void ParseArray(YAML::Node list_node, T *list, int list_len) {
+  int i = 0;
+  for(auto val_node : list_node) {
+    list[i++] = val_node.as<T>();
+  }
+}
+
+template<typename T>
+void ParseVector(YAML::Node list_node, std::vector<T> &list) {
+  int i = 0;
+  for(auto val_node : list_node) {
+    list.emplace_back(val_node.as<T>());
+  }
+}
+
+template<typename T>
+void ParseMatrix(YAML::Node matrix_node, T *matrix, int max_row_len, int max_col_len, int *col_len) {
+  int i = 0;
+  for(auto row : matrix_node) {
+    ParseArray<T>(row, &matrix[i*max_col_len], col_len[i]);
+    ++i;
+  }
+}
+
+template<typename T>
+void ParseMatrix(YAML::Node matrix_node, T *matrix, int max_row_len, int max_col_len) {
+  int i = 0;
+  for(auto row : matrix_node) {
+    ParseArray<T>(row, &matrix[i*max_col_len], max_col_len);
+    ++i;
+  }
+}
+
+void ParseRangeList(YAML::Node list_node, std::vector<int> &list) {
+  int i = 0, min, max;
+  for(auto val_node : list_node) {
+    std::string val = val_node.as<std::string>();
+    if(val.find('-') == std::string::npos) {
+      min = val_node.as<int>();
+      max = min;
+    }
+    else {
+      std::stringstream ss(val);
+      std::string word;
+      std::vector<std::string> words;
+      while(std::getline(ss, word, '-')) {
+        words.push_back(word);
+      }
+      if(words.size() != 2) {
+        LOG(FATAL) << "Invalid range definition " << val << std::endl;
+      }
+      min = std::stoi(words[0]);
+      max = std::stoi(words[1]);
+    }
+    for(int i = min; i <= max; ++i) {
+      list.emplace_back(i);
+    }
+  }
+}
 
 void CheckConstraints(Config *config) {
   // rpc_domain must be present if rpc_protocol is "verbs"
