b0fbcef18712acb033e3d6c29ba1cc3ad3d4daf6
lukemartinlogan <lukemartinlogan@gmail.com>
2022-10-20 09:13:40 -0500

    Add prefetcher test

--- test/prefetch_test.cc
+++ test/prefetch_test.cc
@@ -1,6 +1,14 @@
-//
-// Created by lukemartinlogan on 10/20/22.
-//
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Distributed under BSD 3-Clause license.                                   *
+* Copyright by The HDF Group.                                               *
+* Copyright by the Illinois Institute of Technology.                        *
+* All rights reserved.                                                      *
+*                                                                           *
+* This file is part of Hermes. The full Hermes copyright notice, including  *
+* terms governing use, modification, and redistribution, is contained in    *
+* the COPYING file, which can be found at the top directory. If you do not  *
+* have access to the file, you may request a copy from help@hdfgroup.org.   *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <mpi.h>
 #include "hermes.h"
@@ -61,7 +69,7 @@ int main(int argc, char **argv) {
     std::string blob_name = p.CreateBlobName();
     size_t ret = bkt->Get(blob_name, blob);
     assert(VerifyBlob(blob, i, ret));
-    usleep(500000);
+    // usleep(50000);
   }
   LOG(INFO) << "FINISHED GETTING ALL BLOBS" << std::endl;
 
