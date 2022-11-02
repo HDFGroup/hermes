9276775e222c355df782fe6ae8079589bfd29c00
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-11-02 14:14:06 -0500

    fix Codacy issue.

--- ci/install_docs.sh
+++ ci/install_docs.sh
@@ -37,6 +37,6 @@ make dox >& out.txt
 # cat out.txt | grep warning | grep -v "ignoring unsupported tag"
 popd
 rec="$( grep warning build/out.txt | grep -v "ignoring unsupported tag" |  wc -l )"
-echo $rec
-exit $rec
+echo "$rec"
+exit "$rec"
 
