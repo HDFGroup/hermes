97bf80c9085f0ac035a1e822253992e235b80629
lukemartinlogan <lukemartinlogan@gmail.com>
2022-12-01 09:16:08 -0600

    RPC generator

--- src/rpc_generator/rpc_generator/rpc_generator.py
+++ src/rpc_generator/rpc_generator/rpc_generator.py
@@ -1,30 +1,34 @@
 import sys, os, re
 
 rpc_func_text = """
-u32 target_node = vbucket_id.bits.node_id;
-if (target_node == rpc_->node_id_) {
-  LocalAddBlobIdToVBucket(vbucket_id, blob_id);
-} else {
-  RpcCall<bool>(rpc, target_node, "RemoteAddBlobIdToVBucket", vbucket_id,
-                blob_id);
-}
+{RET} {GLOBAL_NAME}({PARAMS}) {{
+  u32 target_node = {TARGET_NODE};
+  if (target_node == rpc_->node_id_) {{
+    LocalAddBlobIdToVBucket(
+      {PASS_PARAMS});
+  }} else {{
+    RpcCall<bool>(rpc, target_node, "{GLOBAL_NAME}",
+      {PASS_PARAMS});
+  }}
+}}
 """
 
 rpc_lambda_text = """
-auto rpc_unlock_blob = [context](const request &req, BlobID id) {
-  bool result = LocalUnlockBlob(context, id);
+auto {LAMBDA_NAME} = [{CONTEXT}](const request &req, {PARAMS}) {{
+  auto result = {CONTEXT}->{LOCAL_NAME}({PASS_PARAMS});
   req.respond(result);
-};
-rpc_server->define("GetBuffers", rpc_get_buffers);
+}};
+rpc_server->define("{GLOBAL_NAME}", rpc_get_buffers);
 """
 
 class Api:
-    def __init__(self, api_str, target_node, macro):
+    def __init__(self, api_str, target_node, context):
         self.api_str = api_str
-        self.macro = macro
         self.target_node = target_node
+        self.context = context
         self.name = None  # The name of the API
         self.global_name = None  # Name of API without "Local"
+        self.lambda_name = None # Name of lambda caller of API (rpc_{name})
         self.ret = None  # Return value of the API
         self.var_defs = None  # The variables in the API
         self.decompose_prototype(api_str)
@@ -57,6 +61,7 @@ class Api:
             proto = self._clean(re.split("[ ]|(\*+)", proto))
             self.name = proto[-1]
             self.global_name = self.name.replace("Local")
+            self.lambda_name = f"Rpc{self.global_name}"
             self.ret = " ".join(proto[:-1])
         except:
             print(f"Failed to decompose proto name: {proto}")
@@ -96,19 +101,66 @@ class RpcGenerator:
         self.path = path
         self.macro = macro
 
-    def add(self, prototype, target_node):
+    def add(self, prototype, target_node, context):
         if self.path not in self.rpcs:
-            self.rpcs[self.path] = []
-        self.rpcs[self.path].append(Api(prototype, target_node, self.macro))
+            self.rpcs[self.path] = {}
+        if self.macro not in self.rpcs[self.path]:
+            self.rpcs[self.path][self.macro] = []
+        self.rpcs[self.path][self.macro].append(
+            Api(prototype, target_node, context))
 
     def generate(self):
-        for path, rpc in self.rpcs.items():
+        for path, macro_rpcs in self.rpcs.items():
+            class_lines = []
+            with open(path) as fp:
+                class_lines = fp.read().splitlines()
 
+            rpc_lambdas = []
+            for macro, rpcs in macro_rpcs.items():
+                global_rpc_funcs = []
+                i = self.find_macro(macro, class_lines)
+                space = self.get_macro_space(i, class_lines)
 
-    def create_rpc_func(self, name):
-        pass
+                for rpc in rpcs:
+                    self.create_global_rpc_func(rpc, space, global_rpc_funcs)
+                    self.create_rpc_lambda(rpc, space, rpc_lambdas)
 
-    def create_rpc_lambda(self, lines):
-        self.lines.append("[]() {")
-        pass
+                class_lines = class_lines[:i+1] + \
+                              global_rpc_funcs + \
+                              class_lines[i:]
+                class_text = "\n".join(class_lines)
+
+    def find_macro(self, macro, lines):
+        for i, line in enumerate(lines):
+            if macro in line:
+                return i
+
+    def get_macro_space(self, i, lines):
+        return len(lines[i]) - len(lines[i].lstrip())
+
+    def add_space(self, space, text):
+        lines = text.splitlines()
+        for j, line in enumerate(lines):
+            lines[j] = f"{space}{line}"
+        return "\n".join(lines)
+
+    def create_global_rpc_func(self, rpc, space, lines):
+        lines.append(rpc_func_text.format(
+            RET=rpc.ret,
+            GLOBAL_NAME=rpc.global_name,
+            PARAMS=rpc.get_args(),
+            PASS_PARAMS=rpc.pass_params(),
+            TARGET_NODE=rpc.target_node
+        ))
+        self.add_space(space, lines[-1])
+
+    def create_rpc_lambda(self, rpc, space, lines):
+        lines.append(rpc_lambda_text.format(
+            LAMBDA_NAME=rpc.lambda_name,
+            CONTEXT=rpc.context,
+            PARAMS=rpc.get_params(),
+            PASS_PARAMS=rpc.pass_params(),
+            LOCAL_NAME=rpc.name
+        ))
+        self.add_space(space, lines[-1])
 
