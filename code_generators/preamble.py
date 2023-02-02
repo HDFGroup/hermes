0c0cb636ada5c7aa8d5b1e8ca133d8f6283594af
lukemartinlogan <lukemartinlogan@gmail.com>
2023-02-02 08:37:06 -0600

    preamble

--- code_generators/preamble.py
+++ code_generators/preamble.py
@@ -8,7 +8,7 @@ USAGE:
 import sys,os,re
 import pathlib
 
-preamble = """
+hermes_preamble = """
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Distributed under BSD 3-Clause license.                                   *
  * Copyright by The HDF Group.                                               *
@@ -20,14 +20,41 @@ preamble = """
  * the COPYING file, which can be found at the top directory. If you do not  *
  * have access to the file, you may request a copy from help@hdfgroup.org.   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-"""
+""".strip()
+
+labstor_preamble = """
+/*
+ * Copyright (C) 2022  SCS Lab <scslab@iit.edu>,
+ * Luke Logan <llogan@hawk.iit.edu>,
+ * Jaime Cernuda Garcia <jcernudagarcia@hawk.iit.edu>
+ * Jay Lofstead <gflofst@sandia.gov>,
+ * Anthony Kougkas <akougkas@iit.edu>,
+ * Xian-He Sun <sun@iit.edu>
+ *
+ * This file is part of LabStor
+ *
+ * LabStor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+""".strip()
 
 def PrependPreamble(path):
     text = ""
     with open(path, 'r') as fp:
         text = fp.read()
-        if "Copyright (C) 2022  SCS Lab <scslab@iit.edu>" in text:
-            text = text.replace(preamble, "")
+        text = text.replace(hermes_preamble, "")
+        text = text.replace(labstor_preamble, "")
         text = text.strip()
         text = re.sub("//\n// Created by [^\n]*\n//\n", "", text)
         text = preamble.strip() + text + "\n"
