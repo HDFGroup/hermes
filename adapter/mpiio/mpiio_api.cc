3a58133e23cd2cdd49c1f0fd15d2ac70f263b3ad
lukemartinlogan <lukemartinlogan@gmail.com>
2023-02-06 10:13:53 -0600

    Improve MPI-IO logging

--- adapter/mpiio/mpiio_api.cc
+++ adapter/mpiio/mpiio_api.cc
@@ -44,6 +44,7 @@ extern "C" {
  */
 int HERMES_DECL(MPI_Init)(int *argc, char ***argv) {
   LOG(INFO) << "MPI Init intercepted." << std::endl;
+  TRANSPARENT_HERMES
   auto real_api = HERMES_MPIIO_API;
   return real_api->MPI_Init(argc, argv);
 }
@@ -55,11 +56,13 @@ int HERMES_DECL(MPI_Finalize)(void) {
 }
 
 int HERMES_DECL(MPI_Wait)(MPI_Request *req, MPI_Status *status) {
+  LOG(INFO) << "In MPI_Wait." << std::endl;
   auto fs_api = HERMES_MPIIO_FS;
   return fs_api->Wait(req, status);
 }
 
 int HERMES_DECL(MPI_Waitall)(int count, MPI_Request *req, MPI_Status *status) {
+  LOG(INFO) << "In MPI_Waitall." << std::endl;
   auto fs_api = HERMES_MPIIO_FS;
   return fs_api->WaitAll(count, req, status);
 }
@@ -71,9 +74,10 @@ int HERMES_DECL(MPI_File_open)(MPI_Comm comm, const char *filename, int amode,
                                MPI_Info info, MPI_File *fh) {
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "Intercept MPI_File_open for filename: " << filename
+            << " and mode: " << amode << std::endl;
   if (fs_api->IsPathTracked(filename)) {
-    LOG(INFO) << "Intercept MPI_File_open for filename: " << filename
-              << " and mode: " << amode << " is tracked." << std::endl;
+    LOG(INFO) << "Intercept MPI_File_open: " << std::endl;
     AdapterStat stat;
     stat.comm_ = comm;
     stat.amode_ = amode;
@@ -89,7 +93,9 @@ int HERMES_DECL(MPI_File_close)(MPI_File *fh) {
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_seek." << std::endl;
   if (fs_api->IsMpiFpTracked(fh)) {
+    LOG(INFO) << "Intercept MPI_File_seek." << std::endl;
     File f; f.hermes_mpi_fh_ = *fh;
     return fs_api->Close(f, stat_exists);
   }
@@ -100,7 +106,9 @@ int HERMES_DECL(MPI_File_seek)(MPI_File fh, MPI_Offset offset, int whence) {
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_seek." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_seek." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->Seek(f, stat_exists, offset, whence);
   }
@@ -112,6 +120,7 @@ int HERMES_DECL(MPI_File_seek_shared)(MPI_File fh, MPI_Offset offset,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_seek_shared." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
     LOG(INFO) << "Intercept MPI_File_seek_shared offset:" << offset
               << " whence:" << whence << "." << std::endl;
@@ -125,7 +134,9 @@ int HERMES_DECL(MPI_File_get_position)(MPI_File fh, MPI_Offset *offset) {
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_get_position." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_get_position." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     (*offset) = static_cast<MPI_Offset>(fs_api->Tell(f, stat_exists));
     return MPI_SUCCESS;
@@ -138,6 +149,7 @@ int HERMES_DECL(MPI_File_read_all)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read_all." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
     LOG(INFO) << "Intercept MPI_File_read_all." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
@@ -151,7 +163,9 @@ int HERMES_DECL(MPI_File_read_at_all)(MPI_File fh, MPI_Offset offset, void *buf,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read_at_all." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_read_at_all." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->ReadAll(f, stat_exists, buf, offset, count, datatype,
                            status);
@@ -165,7 +179,9 @@ int HERMES_DECL(MPI_File_read_at)(MPI_File fh, MPI_Offset offset, void *buf,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read_at." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_read_at." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->Read(f, stat_exists, buf, offset, count, datatype, status);
   }
@@ -176,7 +192,9 @@ int HERMES_DECL(MPI_File_read)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_read." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     int ret = fs_api->Read(f, stat_exists, buf, count, datatype, status);
     if (stat_exists) return ret;
@@ -189,7 +207,9 @@ int HERMES_DECL(MPI_File_read_ordered)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read_ordered." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_read_ordered." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->ReadOrdered(f, stat_exists, buf, count, datatype, status);
   }
@@ -201,6 +221,7 @@ int HERMES_DECL(MPI_File_read_shared)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_read_shared." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
     LOG(INFO) << "Intercept MPI_File_read_shared." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
@@ -213,6 +234,7 @@ int HERMES_DECL(MPI_File_write_all)(MPI_File fh, const void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_write_all." << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
     LOG(INFO) << "Intercept MPI_File_write_all." << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
@@ -227,7 +249,9 @@ int HERMES_DECL(MPI_File_write_at_all)(MPI_File fh, MPI_Offset offset,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_write_at_all" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_write_at_all" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->WriteAll(f, stat_exists, buf,
                             offset, count, datatype, status);
@@ -243,6 +267,7 @@ int HERMES_DECL(MPI_File_write_at)(MPI_File fh, MPI_Offset offset,
   auto fs_api = HERMES_MPIIO_FS;
   LOG(INFO) << "In MPI_File_write_at" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_write_at" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->Write(f, stat_exists, buf, offset, count, datatype, status);
   }
@@ -255,6 +280,7 @@ int HERMES_DECL(MPI_File_write)(MPI_File fh, const void *buf, int count,
   auto fs_api = HERMES_MPIIO_FS;
   LOG(INFO) << "In MPI_File_write" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_write" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->Write(f, stat_exists, buf, count, datatype, status);
   }
@@ -266,7 +292,9 @@ int HERMES_DECL(MPI_File_write_ordered)(MPI_File fh, const void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_write_shared" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_write_ordered" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->WriteOrdered(f, stat_exists, buf, count, datatype, status);
   }
@@ -278,8 +306,10 @@ int HERMES_DECL(MPI_File_write_shared)(MPI_File fh, const void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_write_shared" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
     // NOTE(llogan): originally WriteOrdered
+    LOG(INFO) << "Intercept MPI_File_write_shared" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     return fs_api->WriteOrdered(f, stat_exists, buf, count, datatype, status);
   }
@@ -295,7 +325,9 @@ int HERMES_DECL(MPI_File_iread_at)(MPI_File fh, MPI_Offset offset, void *buf,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iread_at" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iread_at" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->ARead(f, stat_exists, buf, offset, count, datatype, request);
     return MPI_SUCCESS;
@@ -307,7 +339,9 @@ int HERMES_DECL(MPI_File_iread)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iread" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iread" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->ARead(f, stat_exists, buf, count, datatype, request);
   }
@@ -319,7 +353,9 @@ int HERMES_DECL(MPI_File_iread_shared)(MPI_File fh, void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iread_shared" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iread_shared" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->ARead(f, stat_exists, buf, count, datatype, request);
     return MPI_SUCCESS;
@@ -333,7 +369,9 @@ int HERMES_DECL(MPI_File_iwrite_at)(MPI_File fh, MPI_Offset offset,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iwrite_at" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iwrite_at" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->AWrite(f, stat_exists, buf, offset, count, datatype, request);
     return MPI_SUCCESS;
@@ -347,7 +385,9 @@ int HERMES_DECL(MPI_File_iwrite)(MPI_File fh, const void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iwrite" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iwrite" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->AWrite(f, stat_exists, buf, count, datatype, request);
     return MPI_SUCCESS;
@@ -360,7 +400,9 @@ int HERMES_DECL(MPI_File_iwrite_shared)(MPI_File fh, const void *buf, int count,
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iwrite_shared" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_iwrite_shared" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->AWriteOrdered(f, stat_exists, buf, count, datatype, request);
     return MPI_SUCCESS;
@@ -375,7 +417,9 @@ int HERMES_DECL(MPI_File_sync)(MPI_File fh) {
   bool stat_exists;
   auto real_api = HERMES_MPIIO_API;
   auto fs_api = HERMES_MPIIO_FS;
+  LOG(INFO) << "In MPI_File_iwrite_shared" << std::endl;
   if (fs_api->IsMpiFpTracked(&fh)) {
+    LOG(INFO) << "Intercept MPI_File_sync" << std::endl;
     File f; f.hermes_mpi_fh_ = fh;
     fs_api->Sync(f, stat_exists);
     return 0;
