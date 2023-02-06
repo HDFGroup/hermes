20f1be63ef1a480fd43f463f98a13a8598e45e8c
lukemartinlogan <lukemartinlogan@gmail.com>
2023-02-06 09:19:17 -0600

    Remove unused data structures

--- hermes_shm/include/hermes_shm/util/auto_trace.h
+++ hermes_shm/include/hermes_shm/util/auto_trace.h
@@ -1,6 +1,14 @@
-//
-// Created by lukemartinlogan on 1/11/23.
-//
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_AUTO_TRACE_H_
 #define HERMES_SHM_INCLUDE_HERMES_SHM_UTIL_AUTO_TRACE_H_
--- hermes_shm/test/unit/allocators/test_init.h
+++ hermes_shm/test/unit/allocators/test_init.h
@@ -1,28 +1,14 @@
-/*
- * Copyright (C) 2022  SCS Lab <scslab@iit.edu>,
- * Luke Logan <llogan@hawk.iit.edu>,
- * Jaime Cernuda Garcia <jcernudagarcia@hawk.iit.edu>
- * Jay Lofstead <gflofst@sandia.gov>,
- * Anthony Kougkas <akougkas@iit.edu>,
- * Xian-He Sun <sun@iit.edu>
- *
- * This file is part of HermesShm
- *
- * HermesShm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/>.
- */
-
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef HERMES_SHM_TEST_UNIT_ALLOCATORS_TEST_INIT_H_
 #define HERMES_SHM_TEST_UNIT_ALLOCATORS_TEST_INIT_H_
