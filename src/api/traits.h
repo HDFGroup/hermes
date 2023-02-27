71bb35592844daed443d01735a36ebf9660b7fc5
lukemartinlogan <lukemartinlogan@gmail.com>
2023-02-27 01:55:10 -0600

    vbucket trait interface

--- src/api/traits.h
+++ src/api/traits.h
@@ -13,4 +13,28 @@
 #ifndef HERMES_SRC_API_TRAITS_H_
 #define HERMES_SRC_API_TRAITS_H_
 
+namespace hermes::api {
+
+class VBucket;
+
+class Trait {
+ public:
+  /** Called when AttachTrait is called on VBucket */
+  virtual void Attach(VBucket *vbkt) = 0;
+
+  /** Called when DetachTrait is called on VBucket */
+  virtual void Detach(VBucket *vbkt) = 0;
+
+  /** Called when a Link is called on VBucket*/
+  virtual void OnLink(VBucket *vbkt) = 0;
+
+  /** Called when Unlink is called on VBucket*/
+  virtual void OnUnlink(VBucket *vbkt) = 0;
+
+  /** Called when GetLinks is called on VBucket */
+  virtual void OnGet(VBucket *vbkt) = 0;
+};
+
+}  // namespace hermes::api
+
 #endif  // HERMES_SRC_API_TRAITS_H_
