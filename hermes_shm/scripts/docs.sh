b8b67ec142bac958aad11f5d57e85e99f078bf8e
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-18 15:07:28 -0500

    Re-add hermes_shm

--- hermes_shm/scripts/docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make dox >& out.txt
-# cat out.txt | grep warning | grep -v "ignoring unsupported tag"
-rec="$( grep warning build/out.txt | grep -v "ignoring unsupported tag" |  wc -l )"
-echo "$rec"
-exit "$rec"
-
