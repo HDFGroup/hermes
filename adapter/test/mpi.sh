02fa3324496fb36d0ed5669452137759e3378ee4
lukemartinlogan <lukemartinlogan@gmail.com>
2023-03-24 06:39:39 -0500

    Ensure MPI test passes correct client and server conf in adapter

--- adapter/test/mpi.sh
+++ adapter/test/mpi.sh
@@ -8,24 +8,12 @@ EXEC_NAME=$6
 ARGS=$7
 SLEEP_TIME=3
 
-export HERMES_CLIENT_CONF="${CMAKE_SOURCE_DIR}/test/data/hermes_client.yaml"
-export HERMES_CONF="${CMAKE_SOURCE_DIR}/test/data/hermes_server.yaml"
-
-# Start the Hermes daemon
-echo "STARTING DAEMON"
-${CMAKE_BINARY_DIR}/bin/hermes_daemon &
-echo "WAITING FOR DAEMON"
-sleep ${SLEEP_TIME}
-
 # Run the program
-echo "RUNNING PROGRAM"
+echo "RUNNING PROGRAM (MPI)"
 export LSAN_OPTIONS=suppressions="${CMAKE_SOURCE_DIR}/test/data/asan.supp"
 ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${MPI_PROC} \
 ${CMAKE_BINARY_DIR}/bin/${EXEC_NAME} "${ARGS}" -d yes
 status=$?
-
-# Finalize the Hermes daemon
-${CMAKE_BINARY_DIR}/bin/finalize_hermes
 
 # Exit with status
 exit $status
--- adapter/test/mpi_hermes.sh
+++ adapter/test/mpi_hermes.sh
@@ -3,28 +3,30 @@
 CMAKE_SOURCE_DIR=$1
 CMAKE_BINARY_DIR=$2
 MPI_EXEC=$3
-DAEMON_PROCS=$4
-TEST_PROCS=$5
-EXEC_NAME=$6
-TEST_ARGS=$7
+TEST_PROCS=$4
+EXEC_NAME=$5
+TEST_ARGS=$6
 SLEEP_TIME=3
 
 export HERMES_CLIENT_CONF="${CMAKE_SOURCE_DIR}/test/data/hermes_client.yaml"
 export HERMES_CONF="${CMAKE_SOURCE_DIR}/test/data/hermes_server.yaml"
-export TEST_EXEC="${CMAKE_BINARY_DIR}/bin/${EXEC_NAME}"
-export DAEMON_EXEC="${CMAKE_BINARY_DIR}/bin/hermes_daemon"
+TEST_EXEC="${CMAKE_BINARY_DIR}/bin/${EXEC_NAME}"
+DAEMON_EXEC="${CMAKE_BINARY_DIR}/bin/hermes_daemon"
 
 # Spawn the hermes daemon
 echo "STARTING HERMES DAEMON: ${DAEMON_EXEC}"
-${MPI_EXEC} -n ${DAEMON_PROCS} ${DAEMON_EXEC} ${HERMES_CONF} &
+${DAEMON_EXEC} ${HERMES_CONF} &
 
 # Wait for daemon
-echo "Started hermes daemon with ${DAEMON_PROCS} procs. sleeping for ${SLEEP_TIME} seconds"
+echo "Started hermes daemon. sleeping for ${SLEEP_TIME} seconds"
 sleep ${SLEEP_TIME}
 
 # Run program
-echo "RUNNING PROGRAM: ${EXEC_NAME}"
-${MPI_EXEC} -n ${TEST_PROCS} ${TEST_EXEC} ${TEST_ARGS}
+echo "RUNNING PROGRAM: ${TEST_EXEC}"
+${MPI_EXEC} -n ${TEST_PROCS} \
+-genv HERMES_CLIENT_CONF=${HERMES_CLIENT_CONF} \
+-genv HERMES_CONF=${HERMES_CONF} \
+${TEST_EXEC} ${TEST_ARGS}
 status=$?
 
 # Finalize the Hermes daemon
