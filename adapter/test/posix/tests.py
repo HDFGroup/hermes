6a0d9e919b4dd01cf2c65e7f49e24374959bfb96
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-21 20:11:09 -0500

    Convert unit tests to python instead of bash

---
+++ adapter/test/posix/tests.py
@@ -0,0 +1,102 @@
+from py_hermes_ci.test_manager import TestManager
+from jarvis_util.shell.exec import Exec
+from jarvis_util.shell.local_exec import LocalExecInfo
+
+
+class PosixTestManager(TestManager):
+    def spawn_all_nodes(self):
+        return self.spawn_info()
+
+    def set_paths(self):
+        self.POSIX_CMD = f"{self.CMAKE_BINARY_DIR}/bin/posix_adapter_test"
+        self.HERMES_POSIX_CMD = f"{self.CMAKE_BINARY_DIR}/bin/hermes_posix_adapter_test"
+        self.POSIX_MPI_CMD = f"{self.CMAKE_BINARY_DIR}/bin/posix_adapter_test_mpi"
+        self.HERMES_POSIX_MPI_CMD = f"{self.CMAKE_BINARY_DIR}/bin/hermes_posix_adapter_test_mpi"
+        self.POSIX_SIMPLE_IO_CMD = f"{self.CMAKE_BINARY_DIR}/bin/posix_simple_io"
+        self.HERMES_POSIX_SIMPLE_IO_CMD = f"{self.CMAKE_BINARY_DIR}/bin/hermes_posix_simple_io"
+
+    def test_posix_basic(self):
+        posix_cmd = f"{self.POSIX_CMD}"
+        node = Exec(posix_cmd)
+        return node.exit_code
+
+    def test_hermes_posix_basic_small(self):
+        posix_cmd = f"{self.HERMES_POSIX_CMD} " \
+                    f"~[request_size=range-large]  " \
+                    f"--reporter compact -d yes"
+        spawn_info = self.spawn_info(nprocs=1,
+                                     hermes_conf='hermes_server')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_hermes_posix_basic_large(self):
+        posix_cmd = f"{self.HERMES_POSIX_CMD} " \
+                    f"[request_size=range-large]  " \
+                    f"--reporter compact -d yes"
+        spawn_info = self.spawn_info(nprocs=1,
+                                     hermes_conf='hermes_server')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_posix_basic_mpi(self):
+        pass
+
+    def test_hermes_posix_mpi_small(self):
+        posix_cmd = f"{self.HERMES_POSIX_CMD} " \
+                    f"~[request_size=range-large]  " \
+                    f"--reporter compact -d yes"
+        spawn_info = self.spawn_info(nprocs=2,
+                                     hermes_conf='hermes_server')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_hermes_posix_mpi_large(self):
+        posix_cmd = f"{self.HERMES_POSIX_CMD} " \
+                    f"[request_size=range-large]  " \
+                    f"--reporter compact -d yes"
+        spawn_info = self.spawn_info(nprocs=2,
+                                     hermes_conf='hermes_server')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_hermes_posix_simple_io_omp_default(self):
+        posix_cmd = f"{self.HERMES_POSIX_SIMPLE_IO_CMD} " \
+                    f"/tmp/test_hermes/hi.txt 0 1024 8 0"
+        spawn_info = self.spawn_info(nprocs=2,
+                                     hermes_conf='hermes_server',
+                                     hermes_mode='kDefault')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_hermes_posix_simple_io_omp_scratch(self):
+        posix_cmd = f"{self.HERMES_POSIX_SIMPLE_IO_CMD} " \
+                    f"/tmp/test_hermes/hi.txt 0 1024 8 0"
+        spawn_info = self.spawn_info(nprocs=2,
+                                     hermes_conf='hermes_server',
+                                     hermes_mode='kScratch')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
+
+    def test_hermes_posix_simple_io_omp_preload(self):
+        posix_cmd = f"{self.HERMES_POSIX_SIMPLE_IO_CMD} " \
+                    f"/tmp/test_hermes/hi.txt 0 1024 8 0"
+        spawn_info = self.spawn_info(nprocs=2,
+                                     hermes_conf='hermes_server',
+                                     hermes_mode='kScratch',
+                                     api='posix')
+        self.start_daemon(spawn_info)
+        node = Exec(posix_cmd, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
---
+++ ci/py_hermes_ci/py_hermes_ci/test_manager.py
@@ -0,0 +1,195 @@
+from jarvis_util.jutil_manager import JutilManager
+from jarvis_util.shell.kill import Kill
+from jarvis_util.shell.exec_info import ExecType, ExecInfo
+from jarvis_util.shell.local_exec import LocalExecInfo
+from jarvis_util.shell.mpi_exec import MpiExecInfo
+from jarvis_util.shell.pssh_exec import PsshExecInfo
+from jarvis_util.shell.rm import Rm
+from jarvis_util.shell.exec import Exec
+import time
+import os, sys
+from abc import ABC, abstractmethod
+
+
+class SpawnInfo(MpiExecInfo):
+    def __init__(self, nprocs, hermes_conf=None, hermes_mode=None, api=None,
+                 **kwargs):
+        super().__init__(nprocs=nprocs, **kwargs)
+        self.hermes_conf = hermes_conf
+        self.hermes_mode = hermes_mode
+        self.api = api
+
+
+class TestManager(ABC):
+    """======================================================================"""
+    """ Test Case Constructor """
+    """======================================================================"""
+    def __init__(self, cmake_source_dir, cmake_binary_dir):
+        """
+        Initialize test manager
+        """
+        jutil = JutilManager.get_instance()
+        jutil.collect_output = True
+        jutil.hide_output = False
+        self.CMAKE_SOURCE_DIR = cmake_source_dir
+        self.CMAKE_BINARY_DIR = cmake_binary_dir
+        self.HERMES_TRAIT_PATH = f"{self.CMAKE_BINARY_DIR}/bin"
+        self.HERMES_CLIENT_CONF = f"{self.CMAKE_SOURCE_DIR}/test/data/hermes_client.conf"
+        self.daemon = None
+
+        self.tests_ = {}
+        self.devices = {}
+        self.set_devices()
+        self.find_tests()
+
+    def spawn_info(self, nprocs=None, ppn=None, hostfile=None,
+                   hermes_conf=None, hermes_mode=None, api=None,
+                   file_output=None):
+        # Whether to deploy hermes
+        use_hermes = hermes_mode is not None \
+                     or api == 'native' \
+                     or hermes_conf is not None
+
+        # Get the hermes configuration path
+        if use_hermes:
+            if hermes_conf is None:
+                hermes_conf = os.path.join(self.CMAKE_SOURCE_DIR,
+                                           'test', 'data',
+                                           'hermes_server.yaml')
+            else:
+                hermes_conf = os.path.join(self.CMAKE_SOURCE_DIR,
+                                           'test', 'data',
+                                           f"{hermes_conf}.yaml")
+
+        # Basic environment variables
+        env = {
+            'PATH': os.getenv('PATH'),
+            'LD_LIBRARY_PATH': os.getenv('LD_LIBRARY_PATH')
+        }
+        if use_hermes:
+            env.update({
+                'HERMES_LOG_OUT': "/tmp/hermes_log.txt",
+                'HERMES_CONF': hermes_conf,
+                'HERMES_CLIENT_CONF': self.HERMES_CLIENT_CONF,
+                'HERMES_TRAIT_PATH': self.HERMES_TRAIT_PATH,
+            })
+            if hostfile:
+                env['HERMES_HOSTFILE'] = hostfile.path
+
+        # Hermes interceptor paths
+        if use_hermes:
+            if api == 'posix':
+                env['LD_PRELOAD'] = f"{self.CMAKE_BINARY_DIR}/bin" \
+                                    f"/libhermes_posix.so"
+            elif api == 'stdio':
+                env['LD_PRELOAD'] = f"{self.CMAKE_BINARY_DIR}/bin" \
+                                    f"/libhermes_stdio.so"
+            elif api == 'mpiio':
+                env['LD_PRELOAD'] = f"{self.CMAKE_BINARY_DIR}/bin" \
+                                    f"/libhermes_mpiio.so"
+            elif api == 'hdf5':
+                env['LD_PRELOAD'] = f"{self.CMAKE_BINARY_DIR}/bin" \
+                                    f"/libhdf5_hermes_vfd.so"
+                env['HDF5_PLUGIN_PATH'] = f"{self.CMAKE_BINARY_DIR}/bin"
+
+        # Hermes mode
+        if hermes_mode == 'kDefault':
+            env['HERMES_ADAPTER_MODE'] = 'kDefault'
+        if hermes_mode == 'kScratch':
+            env['HERMES_ADAPTER_MODE'] = 'kScratch'
+        if hermes_mode == 'kBypass':
+            env['HERMES_ADAPTER_MODE'] = 'kBypass'
+
+        return SpawnInfo(nprocs=nprocs,
+                         ppn=ppn,
+                         hostfile=hostfile,
+                         hermes_conf=hermes_conf,
+                         hermes_mode=hermes_mode,
+                         file_output=file_output,
+                         api=api,
+                         env=env)
+
+    @abstractmethod
+    def set_paths(self):
+        pass
+
+    def set_devices(self):
+        self.devices['nvme'] = '/tmp/test_hermes/'
+        self.devices['ssd'] = '/tmp/test_hermes/'
+        self.devices['hdd'] = '/tmp/test_hermes/'
+        self.devices['pfs'] = '/tmp/test_hermes/'
+
+    @abstractmethod
+    def spawn_all_nodes(self):
+        pass
+
+    def cleanup(self):
+        dirs = " ".join([os.path.join(d, '*') for d in self.devices.values()])
+        Rm(dirs, PsshExecInfo(hostfile=self.spawn_all_nodes().hostfile))
+
+    def find_tests(self):
+        # Filter the list to include only attributes that start with "test"
+        test_attributes = [attr for attr in dir(self) if
+                           attr.startswith("test")]
+
+        # Get a reference to each test method
+        for attr in test_attributes:
+            if callable(getattr(self, attr)):
+                self.tests_[attr] = getattr(self, attr)
+
+    def call(self, test_name):
+        self.set_paths()
+        test_name = test_name.strip()
+        if test_name in self.tests_:
+            print(f"Running test: {test_name}")
+            exit_code = self.tests_[test_name]()
+        else:
+            print(f"{test_name} was not found. Available tests: ")
+            for i, test in enumerate(self.tests_):
+                print(f"{i}: {test}")
+            exit_code = 1
+            exit(1)
+        self.cleanup()
+        exit(exit_code)
+
+    """======================================================================"""
+    """ General Test Helper Functions """
+    """======================================================================"""
+
+    def start_daemon(self, spawn_info):
+        """
+        Helper function. Start the Hermes daemon
+
+        :param env: Hermes environment variables
+        :return: None
+        """
+        Kill("hermes_daemon",
+             PsshExecInfo(
+                 hostfile=spawn_info.hostfile,
+                 collect_output=False))
+
+        print("Start daemon")
+        self.daemon = Exec(f"{self.CMAKE_BINARY_DIR}/bin/hermes_daemon",
+                           PsshExecInfo(
+                               hostfile=spawn_info.hostfile,
+                               env=spawn_info.env,
+                               collect_output=False,
+                               hide_output=False,
+                               exec_async=True))
+        time.sleep(30)
+        print("Launched")
+
+    def stop_daemon(self, spawn_info):
+        """
+        Helper function. Stop the Hermes daemon.
+
+        :param env: Hermes environment variables
+        :return: None
+        """
+        print("Stop daemon")
+        Exec(f"{self.CMAKE_BINARY_DIR}/bin/finalize_hermes",
+             LocalExecInfo(
+                 env=spawn_info.env,
+                 collect_output=False))
+        self.daemon.wait()
+        print("Stopped daemon")
