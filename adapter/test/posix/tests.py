8e71984e74332b0c5789008b750a21988827e792
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-25 14:20:34 -0500

    Add posix test back

--- adapter/test/posix/tests.py
+++ adapter/test/posix/tests.py
@@ -15,7 +15,7 @@ class PosixTestManager(TestManager):
         self.POSIX_SIMPLE_IO_CMD = f"{self.CMAKE_BINARY_DIR}/bin/posix_simple_io_omp"
         self.HERMES_POSIX_SIMPLE_IO_CMD = f"{self.CMAKE_BINARY_DIR}/bin/hermes_posix_simple_io_omp"
 
-        self.disable_testing = True
+        self.disable_testing = False
 
     def test_posix_basic(self):
         posix_cmd = f"{self.POSIX_CMD}"
