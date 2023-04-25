6f0569b0e70f4b40555822da095e7fed0f59fee3
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-25 01:29:19 -0500

    Change cmake for hermes install script

--- ci/install_hermes.sh
+++ ci/install_hermes.sh
@@ -22,23 +22,18 @@ set -x
 spack load --only dependencies hermes
 
 # Build hermes
-# export CXXFLAGS="${CXXFLAGS} -std=c++17 -Werror -Wall -Wextra"
+export CXXFLAGS="${CXXFLAGS} -std=c++17 -Werror -Wall -Wextra"
 cmake                                                      \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}               \
     -DCMAKE_PREFIX_PATH=${DEPENDENCY_PREFIX}               \
     -DCMAKE_BUILD_RPATH=${DEPENDENCY_PREFIX}/lib           \
     -DCMAKE_INSTALL_RPATH=${DEPENDENCY_PREFIX}/lib         \
-    -DCMAKE_BUILD_TYPE=${BUILD_TYPE}                       \
-    -DCMAKE_CXX_COMPILER=`which mpicxx`                    \
-    -DCMAKE_C_COMPILER=`which mpicc`                       \
+    -DCMAKE_BUILD_TYPE=Debug                               \
     -DBUILD_SHARED_LIBS=ON                                 \
     -DHERMES_ENABLE_COVERAGE=ON                            \
-    -DHERMES_INTERCEPT_IO=OFF                              \
     -DHERMES_BUILD_BENCHMARKS=ON                           \
-    -DHERMES_COMMUNICATION_MPI=ON                          \
     -DHERMES_BUILD_BUFFER_POOL_VISUALIZER=OFF              \
-    -DORTOOLS_DIR=${DEPENDENCY_PREFIX}                     \
-    -DHERMES_USE_ADDRESS_SANITIZER=OFF                     \
+    -DHERMES_USE_ADDRESS_SANITIZER=ON                      \
     -DHERMES_USE_THREAD_SANITIZER=OFF                      \
     -DHERMES_RPC_THALLIUM=ON                               \
     -DBUILD_TESTING=ON                                     \
@@ -47,7 +42,6 @@ cmake                                                      \
 cmake --build . -- -j4
 
 # Run unit tests
-mkdir /tmp/test_hermes
 ctest -VV
 
 popd
