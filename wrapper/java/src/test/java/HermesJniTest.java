f018521fc9b8709f7175091834b2dd818e290d62
lukemartinlogan <lukemartinlogan@gmail.com>
2023-04-29 02:06:55 -0500

    Integrate gradle into cmake

--- wrapper/java/src/test/java/HermesJniTest.java
+++ wrapper/java/src/test/java/HermesJniTest.java
@@ -1,7 +1,15 @@
 package src.test;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import src.main.java.Blob;
+import src.main.java.Bucket;
+import src.main.java.Hermes;
+import src.main.java.UniqueId;
 
 public class HermesJniTest {
-    public static void main(String[] args) {
+    @Test
+    public void testBucketPutGet() {
         Hermes hermes = Hermes.getInstance();
         hermes.create();
         Bucket bkt = hermes.getBucket("hello");
