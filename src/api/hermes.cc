59c01f4c40b40f943bd6904bd19dc9b09ecf110e
lukemartinlogan <lukemartinlogan@gmail.com>
2023-05-29 21:18:04 -0500

    Try not forcing thread model set

--- src/api/hermes.cc
+++ src/api/hermes.cc
@@ -50,13 +50,13 @@ void Hermes::Init(HermesType mode,
 
 /** Initialize Hermes as a server */
 void Hermes::InitServer(std::string server_config_path) {
+  HERMES_THREAD_MODEL->SetThreadModel(hshm::ThreadType::kArgobots);
   LoadServerConfig(server_config_path);
   InitSharedMemory();
 
   // Initialize RPC
   rpc_.InitServer();
   rpc_.InitClient();
-  HERMES_THREAD_MODEL->SetThreadModel(hshm::ThreadType::kArgobots);
 
   // Load the trait libraries
   traits_.Init();
@@ -78,7 +78,7 @@ void Hermes::InitClient(std::string server_config_path,
   // Initialize references to SHM types
   mdm_.shm_deserialize(header_->mdm_);
   rpc_.InitClient();
-  HERMES_THREAD_MODEL->SetThreadModel(hshm::ThreadType::kArgobots);
+  // HERMES_THREAD_MODEL->SetThreadModel(hshm::ThreadType::kArgobots);
   bpm_.shm_deserialize(header_->bpm_);
   borg_.shm_deserialize(header_->borg_);
   prefetch_.Init();
