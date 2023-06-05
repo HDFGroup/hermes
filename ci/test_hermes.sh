591b8ca8d2919c71127bb1de84b6d7d4ba13ec1e
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-05 11:15:10 -0500

    Nearly done fixing cmake

--- ci/test_hermes.sh
+++ ci/test_hermes.sh
@@ -5,9 +5,24 @@ pushd build
 ctest -VV -R test_hermes_posix_basic_small
 popd build
 
+# Set proper flags for cmake to find Hermes
+INSTALL_PREFIX="${HOME}/hermes_install"
+export LDFLAGS="-L${INSTALL_PREFIX}/lib:${LDFLAGS}"
+export CFLAGS="-I${INSTALL_PREFIX}/include:${CFLAGS}"
+export CPATH="${INSTALL_PREFIX}/include:${CPATH}"
+export CMAKE_PREFIX_PATH="${INSTALL_PREFIX}:${CMAKE_PREFIX_PATH}"
+export CXXFLAGS="-I${INSTALL_PREFIX}/include:${CXXFLAGS}"
+
+unset -f LDFLAGS
+unset -f CFLAGS
+unset -f CXXFLAGS
+unset -f CMAKE_PREFIX_PATH
+unset -f CMAKE_MODULE_PATH
+
 # Run make install unit test
 cd ci/external
 mkdir build
 cd build
 cmake ../
 make -j8
+
