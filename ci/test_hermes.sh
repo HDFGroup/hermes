7348ce7ed20116b6f37d7f7d95192eb0ed9afc99
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-05 17:30:44 -0500

    Set prefix flags for hermes

--- ci/test_hermes.sh
+++ ci/test_hermes.sh
@@ -6,7 +6,7 @@ ctest -VV -R test_hermes_posix_basic_small
 popd build
 
 # Set proper flags for cmake to find Hermes
-INSTALL_PREFIX="${HOME}/hermes_install"
+INSTALL_PREFIX="${HOME}/install"
 export LIBRARY_PATH="${INSTALL_PREFIX}/lib:${LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${INSTALL_PREFIX}/lib:${LD_LIBRARY_PATH}"
 export LDFLAGS="-L${INSTALL_PREFIX}/lib:${LDFLAGS}"
@@ -14,12 +14,6 @@ export CFLAGS="-I${INSTALL_PREFIX}/include:${CFLAGS}"
 export CPATH="${INSTALL_PREFIX}/include:${CPATH}"
 export CMAKE_PREFIX_PATH="${INSTALL_PREFIX}:${CMAKE_PREFIX_PATH}"
 export CXXFLAGS="-I${INSTALL_PREFIX}/include:${CXXFLAGS}"
-
-unset -f LDFLAGS
-unset -f CFLAGS
-unset -f CXXFLAGS
-unset -f CMAKE_PREFIX_PATH
-unset -f CMAKE_MODULE_PATH
 
 # Run make install unit test
 cd ci/external
