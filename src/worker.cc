f67d2302c4185f1f40617358075dd627c0effcba
lukemartinlogan <lukemartinlogan@gmail.com>
2023-09-23 17:11:01 -0500

    don't constantly spawn async threads

--- src/worker.cc
+++ src/worker.cc
@@ -75,7 +75,8 @@ void Worker::PollGrouped(WorkEntry &work_entry) {
         LABSTOR_REMOTE_QUEUE->Disperse(task, exec, ids);
         task->DisableRun();
         task->SetUnordered();
-      } else if (task->IsBlocking() && !task->IsStarted()) {
+      } else if (task->IsBlocking()) {
+        task->DisableRun();
         task->SetStarted();
         entry->thread_ = LABSTOR_WORK_ORCHESTRATOR->SpawnAsyncThread(&Worker::RunBlocking, task);
       } else {
