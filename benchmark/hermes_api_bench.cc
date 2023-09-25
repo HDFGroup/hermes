b1d8f8ebffdd412b2e704861498d0f988850b0f1
lukemartinlogan <lukemartinlogan@gmail.com>
2023-09-24 23:58:28 -0500

    Include blob size

--- benchmark/hermes_api_bench.cc
+++ benchmark/hermes_api_bench.cc
@@ -68,7 +68,7 @@ void GetTest(int nprocs, int rank,
   t.Resume();
   for (int j = 0; j < repeat; ++j) {
     for (size_t i = 0; i < blobs_per_rank; ++i) {
-      hermes::Blob ret;
+      hermes::Blob ret(blob_size);
       size_t blob_name_int = rank * blobs_per_rank + i;
       std::string name = std::to_string(blob_name_int);
       bkt.Get(name, ret, ctx);
