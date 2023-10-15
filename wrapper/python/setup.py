f48679cbef0c8f7af720717dd4ed1d3bfb08c1ee
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-15 06:15:00 -0500

    Change wrapper api

--- wrapper/python/setup.py
+++ wrapper/python/setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
 setuptools.setup(
-    name="degenerate_studio",
+    name="hermes",
     packages=setuptools.find_packages(),
     version="0.0.1",
     author="Luke Logan",
     author_email="lukemartinlogan@gmail.com",
-    description="A graphics library",
-    url="https://github.com/scs-lab/jarvis-util",
+    description="An I/O buffering library",
+    url="https://github.com/HDFGroup/hermes.git",
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 0 - Pre-Alpha",
+        "Development Status :: 1.1",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: None",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Graphics",
+        "Topic :: I/O",
     ],
     long_description=""
 )
