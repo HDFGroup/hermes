7dfe1b2c1d5e1e5083b7213643c78dded1c50312
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-19 00:37:05 -0500

    Use stager mdm only

--- include/hermes/config_manager.h
+++ include/hermes/config_manager.h
@@ -51,8 +51,8 @@ class ConfigurationManager {
     blob_mdm_.CreateRoot(DomainId::GetGlobal(), "hermes_blob_mdm");
     bkt_mdm_.CreateRoot(DomainId::GetGlobal(), "hermes_bkt_mdm");
     // TODO(llogan): add back
-    op_mdm_.CreateRoot(DomainId::GetGlobal(), "hermes_op_mdm",
-                       bkt_mdm_.id_, blob_mdm_.id_);
+//    op_mdm_.CreateRoot(DomainId::GetGlobal(), "hermes_op_mdm",
+//                       bkt_mdm_.id_, blob_mdm_.id_);
     stager_mdm_.CreateRoot(DomainId::GetGlobal(),
                            "hermes_stager_mdm", blob_mdm_.id_);
     blob_mdm_.SetBucketMdmRoot(DomainId::GetGlobal(),
