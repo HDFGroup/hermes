5c186791e5de18d1c482b31c36e963edb8f61841
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-19 02:01:54 -0500

    Print out existing states?

--- hrun/src/worker.cc
+++ hrun/src/worker.cc
@@ -77,6 +77,9 @@ void Worker::PollGrouped(WorkEntry &work_entry) {
     TaskState *&exec = rctx.exec_;
     exec = HRUN_TASK_REGISTRY->GetTaskState(task->task_state_);
     if (!exec) {
+      for (std::pair<std::string, TaskStateId> entries  : HRUN_TASK_REGISTRY->task_state_ids_) {
+        HILOG(kInfo, "Task state: {} id: {}", entries.first, entries.second)
+      }
       HELOG(kFatal, "(node {}) Could not find the task state: {}",
             HRUN_CLIENT->node_id_, task->task_state_);
       entry->complete_ = true;
