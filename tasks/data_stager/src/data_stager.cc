b3041867f25c2f95442978e31843e3cd5ea90239
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-19 14:51:51 -0500

    Comment out stage in

--- tasks/data_stager/src/data_stager.cc
+++ tasks/data_stager/src/data_stager.cc
@@ -54,7 +54,7 @@ class Server : public TaskLib {
       task->SetModuleComplete();
     }
     AbstractStager &stager = *it->second;
-    stager.StageIn(blob_mdm_, task, rctx);
+    // stager.StageIn(blob_mdm_, task, rctx);
     task->SetModuleComplete();
   }
 
