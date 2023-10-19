0ff14bef8e9f8b0d994af0835a81dcfe2796ed3f
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-19 00:52:08 -0500

    Comment out part of constructor for data_op

--- tasks/hermes_data_op/src/hermes_data_op.cc
+++ tasks/hermes_data_op/src/hermes_data_op.cc
@@ -35,10 +35,10 @@ class Server : public TaskLib {
     bkt_mdm_.Init(task->bkt_mdm_);
     blob_mdm_.Init(task->blob_mdm_);
     client_.Init(id_);
-    op_id_map_["min"] = 0;
-    op_id_map_["max"] = 1;
-    op_graphs_.resize(HRUN_QM_RUNTIME->max_lanes_);
-    run_task_ = client_.AsyncRunOp(task->task_node_ + 1);
+//    op_id_map_["min"] = 0;
+//    op_id_map_["max"] = 1;
+//    op_graphs_.resize(HRUN_QM_RUNTIME->max_lanes_);
+//    run_task_ = client_.AsyncRunOp(task->task_node_ + 1);
     task->SetModuleComplete();
   }
 
