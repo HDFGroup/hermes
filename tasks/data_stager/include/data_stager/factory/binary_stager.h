749a3c83249363495314f1d68fd39134d77bfb16
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-19 23:14:02 -0500

    Include bkt_id

--- tasks/data_stager/include/data_stager/factory/binary_stager.h
+++ tasks/data_stager/include/data_stager/factory/binary_stager.h
@@ -73,7 +73,7 @@ class BinaryFileStager : public AbstractStager {
     memcpy(blob.ptr_ + plcmnt.blob_off_, blob.ptr_, real_size);
     HILOG(kInfo, "Staged {} bytes from the backend file {}",
           real_size, url_);
-    HILOG(kInfo, "Submitting put blob {} to blob mdm", task->blob_name_->str())
+    HILOG(kInfo, "Submitting put blob {} ({}) to blob mdm", task->blob_name_->str(), task->bkt_id_)
     hapi::Context ctx;
     LPointer<blob_mdm::PutBlobTask> put_task =
         blob_mdm.AsyncPutBlob(task->task_node_ + 1,
