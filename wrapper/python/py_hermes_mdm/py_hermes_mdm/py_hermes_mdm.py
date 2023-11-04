81ed42398c0c92f97a7dd8865e9ebeaff3017848
lukemartinlogan <lukemartinlogan@gmail.com>
2023-11-04 05:45:22 -0500

    Metadata query works single-node

--- wrapper/python/py_hermes_mdm/py_hermes_mdm/py_hermes_mdm.py
+++ wrapper/python/py_hermes_mdm/py_hermes_mdm/py_hermes_mdm.py
@@ -10,14 +10,17 @@ class MetadataSnapshot:
 
     @staticmethod
     def unique(id):
-        return f'{id.unique}.{id.node_id}'
+        return f'{id.node_id}.{id.unique}'
 
     def collect(self):
         mdm = self.hermes.CollectMetadataSnapshot()
+        tag_to_blob = {}
         for blob in mdm.blob_info:
             blob_info = {
                 'name': str(blob.get_name()),
                 'id': self.unique(blob.blob_id),
+                'mdm_node': int(blob.blob_id.node_id),
+                'tag_id': self.unique(blob.tag_id),
                 'score': float(blob.score),
                 'access_frequency': 0,
                 'buffer_info': []
@@ -29,17 +32,23 @@ class MetadataSnapshot:
                 }
                 blob_info['buffer_info'].append(buf_info)
             self.blob_info.append(blob_info)
+            tag_to_blob[blob_info['tag_id']] = blob_info['id']
         for tag in mdm.bkt_info:
             tag_info = {
                 'id': self.unique(tag.tag_id),
+                'mdm_node': int(tag.tag_id.node_id),
                 'name': str(tag.get_name()),
-                'blobs': [self.unique(blob.blob_id) for blob in tag.blobs]
+                # 'blobs': [self.unique(blob.blob_id) for blob in tag.blobs]
+                'blobs': []
             }
+            if tag_info['id'] in tag_to_blob:
+                tag_info['blobs'] = tag_to_blob[tag_info['id']]
             self.tag_info.append(tag_info)
         for target in mdm.target_info:
             target_info = {
                 'name': None,
                 'id':  self.unique(target.tgt_id),
+                'node_id': int(target.tgt_id.node_id),
                 'rem_cap': target.rem_cap,
                 'max_cap': target.max_cap,
                 'bandwidth': target.bandwidth,
