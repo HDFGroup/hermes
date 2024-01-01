cf68b917570796f396f09b822ac6ea001b7fc47c
lukemartinlogan <lukemartinlogan@gmail.com>
2024-01-01 02:52:55 -0600

    Add rxm to fabric

--- ci/hermes/packages/hermes_shm/package.py
+++ ci/hermes/packages/hermes_shm/package.py
@@ -26,7 +26,7 @@ class HermesShm(CMakePackage):
     depends_on('libaio')
     depends_on('doxygen')  # @1.9.3
     depends_on('boost@1.7: +context +fiber +filesystem +system +atomic +chrono +serialization +signals +pic +regex')
-    depends_on('libfabric fabrics=sockets,tcp,udp,verbs,mlx',
+    depends_on('libfabric fabrics=fabrics=sockets,tcp,udp,verbs,mlx,rxm',
                when='+ares')
     depends_on('libfabric fabrics=verbs',
                when='+only_verbs')
