d666a76e97dd91b987d468af5a055030b21a640e
lukemartinlogan <lukemartinlogan@gmail.com>
2024-02-23 07:03:30 -0600

    Add h5py test

---
+++ test/unit/hermes_adapters/vfd/hermes_vfd_py_test.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import h5py
+
+path = 'hello.h5'
+f = h5py.File(path, "w")
+prefix = 'train'
+for p in prefix:
+    group = f.create_group(p)
+    story_dict = {0:1, 1:2, 2:3}
+    length = len(story_dict)
+    images = list()
+    for i in range(5):
+        images.append(
+            group.create_dataset('image{}'.format(i), (length,), dtype=h5py.vlen_dtype(np.dtype('uint8'))))
+    sis = group.create_dataset('sis', (length,), dtype=h5py.string_dtype(encoding='utf-8'))
+    dii = group.create_dataset('dii', (length,), dtype=h5py.string_dtype(encoding='utf-8'))
+f.close()
